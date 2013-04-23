128b5c63b9ec9f66072262947fbfb249d09e20e6
acervantes <acerva01@calpoly.edu>
2013-04-23 10:51:47 -0700

    Sql skeleton

---
+++ DB-cleanup.sql
@@ -0,0 +1,23 @@
+--  Slightly Blue Team
+-- 
+--  Alejandro Cervantes (acerva01)
+--  Corey Farwell
+--  Kyle Sletten
+--  Lance Tyler
+--  Ray Tam
+-- 
+--  CPE366 - Spring 2013
+--  Lab 3
+
+
+DROP TABLE Location;
+
+DROP TABLE Bin;
+
+DROP TABLE Fulfiller;
+
+DROP TABLE Item;
+
+DROP TABLE Manufacturer;
+
+DROP TABLE Catalog;
