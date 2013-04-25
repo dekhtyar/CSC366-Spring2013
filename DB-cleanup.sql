eec99b812ddebf9bf4d45774358eaedd20accba2
chaching976 <chaching976@gmail.com>
2013-04-25 10:28:06 -0700

    Cleanup file for out SQL database

---
+++ DB-cleanup.sql
@@ -0,0 +1,11 @@
+-- Raymond Ching, James Aldag, Brandon Tanaka, Aaron Phung, Jack Henderson
+-- CPE 366 
+-- Lab 3 
+-- Database Cleanup
+
+Drop Table Locations CASCADE CONSTRAINTS;
+Drop Table Bins CASCADE CONSTRAINTS;
+Drop Table BinProduct CASCADE CONSTRAINTS;
+Drop Table Products CASCADE CONSTRAINTS;
+Drop Table FulfillerProduct CASCADE CONSTRAINTS;
+Drop Table LocationProduct CASCADE CONSTRAINTS;
