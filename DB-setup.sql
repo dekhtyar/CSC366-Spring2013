f3a25cc36b4bb3249de2260cd327897a03238eb6
Kyle Sletten <kbsletten@gmail.com>
2013-04-24 20:50:28 -0700

    added StoreItem, OnHand, and Allocation (previously Order)

--- DB-setup.sql
+++ DB-setup.sql
@@ -9,6 +9,17 @@
 --  CPE366 - Spring 2013
 --  Lab 3
 
+CREATE TABLE Manufacturer (
+
+);
+
+CREATE TABLE Catalog (
+
+);
+
+CREATE TABLE Fulfiller (
+    Id      INT PRIMARY KEY
+);
 
 CREATE TABLE Location (
     ExternalId          INT PRIMARY KEY,
@@ -22,29 +33,47 @@ CREATE TABLE Location (
     ManufacturerID      INT,  -- /stocks info
     ItemUPC             VARCHAR2(20), -- These foreign keys reference a StoreItem
     ItemSKU             VARCHAR2(20),
-    FulfillerID         VARCHAR2(20), 
-    FOREIGN KEY (ItemUPC, ItemSKU, FulfillerID) REFERENCES StoreItem  
+    FulfillerID         VARCHAR2(20),
+    FOREIGN KEY (ManufacturerID, CatalogID) REFERENCES Catalog,
+    FOREIGN KEY (FulfillerID) REFERENCES Fulfiller
 );
-
-
 
 
 CREATE TABLE Bin (
-);
-
-
-CREATE TABLE Fulfiller (
-    Id      INT PRIMARY KEY
 );
 
 CREATE TABLE Item (
     UPC     INT PRIMARY KEY
 );
 
-CREATE TABLE Manufacturer (
-
+CREATE TABLE StoreItem (
+    FulfillerID INT,
+    SKU         INT,
+    UPC         INT,
+    PRIMARY KEY (FulfillerID, SKU),
+    FOREIGN KEY (UPC) REFERENCES Item
 );
 
-CREATE TABLE Catalog (
+CREATE TABLE OnHand (
+    FulfillerID INT,
+    ExternalID  INT,
+    Name        VARCHAR2(20),
+    SKU         INT,
+    Count       INT,
+    Allocated   INT,
+    PRIMARY KEY (FulfillerID, ExternalID, Name, SKU),
+    FOREIGN KEY (FulfillerID, ExternalID, Name) REFERENCES Bin,
+    FOREIGN KEY (FulfillerID, SKU) REFERENCES StoreItem
+);
 
+CREATE TABLE Allocation (
+    OrderId     INT PRIMARY KEY,
+    FulfillerID INT,
+    ExternalID  INT,
+    Name        VARCHAR2(20),
+    SKU         INT,
+    CreatedOn   DATE,
+    Quantity    INT,
+    FOREIGN KEY (FulfillerID, ExternalID, Name) REFERENCES Bin,
+    FOREIGN KEY (FulfillerID, ExternalID, Name, SKU) REFERENCES OnHand
 );
