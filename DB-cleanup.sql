f732bd32558b08f71ad3ce3f188f68c947be336c
rossmckelvie <r@rossmckelvie.com>
2013-04-25 10:09:02 -0700

    add DB-cleanup.sql to drop tables

---
+++ DB-cleanup.sql
@@ -0,0 +1,19 @@
+--Team Ross
+-----------
+--Ian McBride
+--Ross McKelvie
+--Matt Tondreau
+--Matt Schirle
+--Riley Lew
+
+DROP TABLE Manufacturers;
+DROP TABLE Catalog;
+DROP TABLE Locations;
+DROP TABLE Offers;
+DROP TABLE Fulfillers;
+DROP TABLE FulfillerCarries;
+DROP TABLE Products;
+DROP TABLE Bins;
+DROP TABLE Contains;
+DROP TABLE LocationSells;
+DROP TABLE Indexes;
