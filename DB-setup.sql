46e6f0503b5420dcd5c466039b3afb1d0635a099
rossmckelvie <r@rossmckelvie.com>
2013-04-25 19:08:17 -0700

    remove blank lines for sqlplus

--- DB-setup.sql
+++ DB-setup.sql
@@ -8,14 +8,12 @@
 
 create table Manufacturers (
   manufacturerId varchar2(10) NOT NULL,
-
   constraint manufacturers_pk PRIMARY KEY (manufacturerId)
 );
 
 create table Catalogs (
   catalogId varchar2(10) NOT NULL,
   manufacturerId varchar2(10) NOT NULL,
-
   constraint catalogs_pk PRIMARY KEY (catalogId, manufacturerId),
   constraint catalogs_fk FOREIGN KEY (manufacturerId) REFERENCES Manufacturers
 );
@@ -29,7 +27,6 @@ create table Locations (
   longitude decimal(9,6),
   status varchar2(50),
   safetyStockLimitDefault varchar2(10),
-
   constraint locations_pk PRIMARY KEY (locationId, fulfillerId),
   constraint locations_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers
 );
@@ -38,7 +35,6 @@ create table LocationOffersCatalogs (
   catalogId varchar2(10) NOT NULL,
   locationId varchar2(10) NOT NULL,
   fulfillerId varchar2(10) NOT NULL,
-
   constraint catalog_fk FOREIGN KEY (catalogId) REFERENCES Catalogs,
   constraint location_fk FOREIGN KEY (locationId) REFERENCES Locations,
   constraint fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers
@@ -46,7 +42,6 @@ create table LocationOffersCatalogs (
 
 create table Fulfillers (
   fulfillerId varchar2(10) NOT NULL,
-
   constraint fulfillers_pk PRIMARY KEY (fulfillerId)
 );
 
@@ -54,7 +49,6 @@ create table FulfillerCarriesProducts (
   fulfillerId varchar2(10) NOT NULL,
   productUpc varchar2(10) NOT NULL,
   sku varchar2(10) NOT NULL,
-
   constraint fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers,
   constraint productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products(upc)
 );
@@ -64,7 +58,6 @@ create table Products (
   catalogId varchar2(10) NOT NULL,
   manufacturerId varchar2(10) NOT NULL,
   name varchar2(50),
-
   constraint products_pk PRIMARY KEY (upc),
   constraint catalog_fk FOREIGN KEY (catalogId) REFERENCES Catalogs,
   constraint manuf_fk FOREIGN KEY (manufacturerId) REFERENCES Manufacturers
@@ -76,7 +69,6 @@ create table Bins (
   name varchar2(50) NOT NULL,
   binType varchar2(50),
   status varchar2(10),
-
   constraint bins_pk PRIMARY KEY (locationId, fulfillerId, name),
   constraint location_fk FOREIGN KEY (locationId) REFERENCES Locations,
   constraint fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers
@@ -89,7 +81,6 @@ create table BinContainsProducts (
   locationId varchar2(10) NOT NULL
   allocated varchar2(6),
   onHand varchar2(6),
-
   constraint productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products(upc),
   constraint binname_fk FOREIGN KEY (binName) REFERENCES Bins(name),
   constraint fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers,
@@ -103,7 +94,6 @@ create table LocationSellsProducts (
   ltd varchar2(10),
   storeSku varchar2(10),
   safetyStock varchar2(10),
-
   constraint productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products(upc),
   constraint fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers,
   constraint location_fk FOREIGN KEY (locationId) REFERENCES Locations
