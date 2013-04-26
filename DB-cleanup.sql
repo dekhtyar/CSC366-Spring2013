17148a2503ca11812d3d3fe8474bc1a7f8519e9d
rossmckelvie <r@rossmckelvie.com>
2013-04-25 19:19:05 -0700

    move fulfillers drop down

--- DB-cleanup.sql
+++ DB-cleanup.sql
@@ -7,12 +7,12 @@
 --Riley Lew
 
 DROP TABLE LocationOffersCatalogs;
-DROP TABLE Fulfillers;
 DROP TABLE FulfillerCarriesProducts;
 DROP TABLE BinContainsProducts;
 DROP TABLE LocationSellsProducts;
 DROP TABLE Products;
 DROP TABLE Bins;
+DROP TABLE Fulfillers;
 DROP TABLE Catalogs;
 DROP TABLE Manufacturers;
 DROP TABLE Locations;
