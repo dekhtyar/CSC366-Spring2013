f0b5c32ba2a396d7ce490c37afbad05bbe1a3027
rossmckelvie <r@rossmckelvie.com>
2013-04-25 19:14:32 -0700

    adjust cleanup order

--- DB-cleanup.sql
+++ DB-cleanup.sql
@@ -6,13 +6,13 @@
 --Matt Schirle
 --Riley Lew
 
-DROP TABLE Manufacturers;
-DROP TABLE Catalogs;
-DROP TABLE Locations;
 DROP TABLE LocationOffersCatalogs;
 DROP TABLE Fulfillers;
 DROP TABLE FulfillerCarriesProducts;
-DROP TABLE Products;
-DROP TABLE Bins;
 DROP TABLE BinContainsProducts;
 DROP TABLE LocationSellsProducts;
+DROP TABLE Products;
+DROP TABLE Bins;
+DROP TABLE Catalogs;
+DROP TABLE Manufacturers;
+DROP TABLE Locations;
