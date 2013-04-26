0ec9e8c3662e52f4a87eb6a653151a950fd428c3
eloui <egloui@yahoo.com>
2013-04-25 17:56:13 -0700

    Created sql-setup and cleanup that create the tables properly.

---
+++ DB-cleanup.sql
@@ -0,0 +1,19 @@
+/* Team GLADE:
+   George Reyes
+   Lauren Thurston
+   Adin Miller
+   Diego Torres
+   Elizabeth Loui
+
+   DB-cleanup.sql */
+
+DROP TABLE containedInBin;
+DROP TABLE locationProduct;
+DROP TABLE retailerProduct;
+DROP TABLE storeBin;
+DROP TABLE catalogServedByLocation;
+DROP TABLE retail_Location;
+DROP TABLE inventoryRecord;
+DROP TABLE catalog;
+DROP TABLE manufacturer;
+DROP TABLE retailer;
