9ce2d63d2b9b3de6d2f7fbe724c024105abc9321
Luke Larson <lplarson@calpoly.edu>
2013-05-02 21:22:16 -0700

    Added files for revision

---
+++ lab4/DB-cleanup.sql
@@ -0,0 +1,17 @@
+--Kevin Stein  kestein@calpoly.edu
+--Alex Spotnitz  aspotnitz@gmail.com
+--Stephen Calabrese  sccalabr@calpoly.edu
+--Alex Boltunov  aboltunov@gmail.com
+--Luke Larson  lplarson@calpoly.edu
+
+DROP TABLE SubscribeTo CASCADE CONSTRAINTS;
+DROP TABLE Catalogues CASCADE CONSTRAINTS;
+DROP TABLE Manufacturers CASCADE CONSTRAINTS;
+DROP TABLE StoredAt CASCADE CONSTRAINTS;
+DROP TABLE StoredIn CASCADE CONSTRAINTS;
+DROP TABLE Bins CASCADE CONSTRAINTS;
+DROP TABLE Items CASCADE CONSTRAINTS;
+DROP TABLE Locations CASCADE CONSTRAINTS;
+DROP TABLE Fulfillers CASCADE CONSTRAINTS;
+DROP TABLE FulfilledBy CASCADE CONSTRAINTS;
+DROP TABLE ListedIn CASCADE CONSTRAINTS;
---
+++ lab4/DB-setup.sql
@@ -0,0 +1,91 @@
+--Kevin Stein  kestein@calpoly.edu
+--Alex Spotnitz  aspotnitz@gmail.com
+--Stephen Calabrese  sccalabr@calpoly.edu
+--Alex Boltunov  aboltunov@gmail.com
+--Luke Larson  lplarson@calpoly.edu
+
+CREATE TABLE Manufacturers (
+   ManufacturerId           VARCHAR2(50) PRIMARY KEY
+);
+
+CREATE TABLE Catalogues (
+   CatalogueId              VARCHAR2(50) PRIMARY KEY,
+   ManufacturerId           VARCHAR2(50) REFERENCES Manufacturers
+);
+
+CREATE TABLE Fulfillers (
+   FulfillerId              VARCHAR2(50) PRIMARY KEY
+);
+
+CREATE TABLE Locations (
+   FulfillerId              VARCHAR2(50) REFERENCES Fulfillers,
+   FulfillerLocationId      VARCHAR2(50),
+   Name                     VARCHAR2(20),
+   Type                     VARCHAR2(20),
+   Latitude                 FLOAT,
+   Longitude                FLOAT,
+   Status                   INTEGER,
+   DefaultSafetyStockLimit  INTEGER,
+   PRIMARY KEY(FulfillerId, FulfillerLocationId),
+   UNIQUE(Latitude, Longitude)   
+);
+
+CREATE TABLE Items (
+   UPC                      CHAR(12)     PRIMARY KEY, 
+   Name                     VARCHAR2(80) 
+);
+
+CREATE TABLE Bins (
+   Name                     VARCHAR2(20),
+   FulfillerId              VARCHAR2(50) REFERENCES Fulfillers,
+   FulfillerLocationId      VARCHAR2(50),
+   PRIMARY KEY(Name, FulfillerId, FulfillerLocationId),
+   FOREIGN KEY (FulfillerId, FulfillerLocationId)
+    REFERENCES Locations(FulfillerId, FulfillerLocationId)
+);
+
+CREATE TABLE StoredIn (
+   UPC                      CHAR(12)     REFERENCES Items,
+   FulfillerId              VARCHAR2(50) REFERENCES Fulfillers,
+   FulfillerLocationId      VARCHAR2(50),
+   Name                     VARCHAR2(20),
+   OnHand                   INTEGER,
+   Allocated                INTEGER,
+   PRIMARY KEY(UPC, FulfillerId, FulfillerLocationId, Name),
+   FOREIGN KEY (Name, FulfillerId, FulfillerLocationId)
+    REFERENCES Bins(Name, FulfillerId, FulfillerLocationId)
+);
+
+CREATE TABLE StoredAt (
+   UPC                      CHAR(12)     REFERENCES Items,
+   FulfillerId              VARCHAR2(50) REFERENCES Fulfillers,
+   FulfillerLocationId      VARCHAR2(50),
+   LTD                      FLOAT,
+   SafetyStockLimit         INTEGER,
+   PRIMARY KEY(UPC, FulfillerId, FulfillerLocationId),
+   FOREIGN KEY (FulfillerId, FulfillerLocationId)
+    REFERENCES Locations(FulfillerId, FulfillerLocationId)
+); 
+
+CREATE TABLE SubscribeTo (
+   FulfillerId              VARCHAR2(50) REFERENCES Fulfillers,
+   FulfillerLocationId      VARCHAR2(50),
+   CatalogueId              VARCHAR2(50) REFERENCES Catalogues,
+   PRIMARY KEY(FulfillerId, FulfillerLocationId, CatalogueId),
+   FOREIGN KEY (FulfillerId, FulfillerLocationId)
+    REFERENCES Locations(FulfillerId, FulfillerLocationId)
+);
+
+CREATE TABLE FulfilledBy (
+   UPC                      CHAR(12)     REFERENCES Items,
+   FulfillerId              VARCHAR2(50) REFERENCES Fulfillers,
+   SKU                      VARCHAR2(50),
+   PRIMARY KEY(UPC, FulfillerId)
+);
+
+CREATE TABLE ListedIn (
+   UPC                      CHAR(12)     REFERENCES Items,
+   CatalogueId              VARCHAR2(50) REFERENCES Catalogues,
+   PRIMARY KEY(UPC, CatalogueId)
+);
+
