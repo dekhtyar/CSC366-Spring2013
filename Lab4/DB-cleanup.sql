148186f55cf7c2875d6dd0e689e3f65ad746643a
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-05-05 21:44:43 +0000

    Added fix to lab 4

--- Lab4/DB-cleanup.sql
+++ Lab4/DB-cleanup.sql
@@ -1,9 +1,15 @@
-DROP TABLE StoredIn;
-DROP TABLE HeldAt;
-DROP TABLE FulfillerSpecificProduct;
-DROP TABLE Bin;
-DROP TABLE Product;
-DROP TABLE Catalog;
-DROP TABLE Location;
-DROP TABLE Manufacturer;
-DROP TABLE Fulfiller;
+USE inventory;
+
+SET foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS StoredIn;
+DROP TABLE IF EXISTS HeldAt;
+DROP TABLE IF EXISTS FulfillerSpecificProduct;
+DROP TABLE IF EXISTS Bin;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Catalog;
+DROP TABLE IF EXISTS Location;
+DROP TABLE IF EXISTS Manufacturer;
+DROP TABLE IF EXISTS Fulfiller;
+
+SET foreign_key_checks = 1;
--- Lab4/DB-setup.sql
+++ Lab4/DB-setup.sql
@@ -1,16 +1,19 @@
-CREATE TABLE Fulfiller (
+CREATE DATABASE IF NOT EXISTS inventory;
+USE inventory;
+
+CREATE TABLE IF NOT EXISTS Fulfiller (
+   id INT,
+
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Manufacturer (
    id VARCHAR(100),
 
    PRIMARY KEY (id)
 );
 
-CREATE TABLE Manufacturer (
-   id VARCHAR(100),
-
-   PRIMARY KEY (id)
-);
-
-CREATE TABLE Location (
+CREATE TABLE IF NOT EXISTS Location (
    ext_ful_loc_id VARCHAR(100),
    int_ful_loc_id INT,
    fulfiller_id INT,
@@ -25,7 +28,7 @@ CREATE TABLE Location (
    FOREIGN KEY (fulfiller_id) REFERENCES Fulfiller(id)
 );
 
-CREATE TABLE Catalog (
+CREATE TABLE IF NOT EXISTS Catalog (
    id VARCHAR(100),
    manufacturer_id VARCHAR(100),
 
@@ -33,70 +36,70 @@ CREATE TABLE Catalog (
    FOREIGN KEY (manufacturer_id) REFERENCES Manufacturer(id)
 );
 
-CREATE TABLE FulfillFor (
-   fulfiller_id VARCHAR(100),
-   ext_ful_loc_id INT,
+CREATE TABLE IF NOT EXISTS FulfillFor (
+   fulfiller_id INT,
+   ext_ful_loc_id VARCHAR(100),
    catalog_id VARCHAR(100),
    manufacturer_id VARCHAR(100),
 
    PRIMARY KEY (fulfiller_id, ext_ful_loc_id, catalog_id, manufacturer_id),
-   FOREIGN KEY (ext_ful_loc_id, fulfiller_id) REFERENCES Location,
-   FOREIGN KEY (catalog_id, manufacturer_id) REFERENCES Catalog
+   FOREIGN KEY (ext_ful_loc_id, fulfiller_id) REFERENCES Location(ext_ful_loc_id, fulfiller_id),
+   FOREIGN KEY (catalog_id, manufacturer_id) REFERENCES Catalog(id, manufacturer_id)
 );
 
-CREATE TABLE Product (
+CREATE TABLE IF NOT EXISTS Product (
    upc VARCHAR(12),
    catalog_id VARCHAR(100),
    manufacturer_id VARCHAR(100),
    name VARCHAR(1),
 
    PRIMARY KEY (upc),
-   FOREIGN KEY (catalog_id, manufacturer_id) REFERENCES Catalog
+   FOREIGN KEY (catalog_id, manufacturer_id) REFERENCES Catalog(id, manufacturer_id)
 );
 
-CREATE TABLE Bin (
+CREATE TABLE IF NOT EXISTS Bin (
    name VARCHAR(100),
-   fulfiller_id VARCHAR(100),
-   ext_ful_loc_id INT,
+   fulfiller_id INT, 
+   ext_ful_loc_id VARCHAR(100),
    type VARCHAR(100),
    status VARCHAR(100),
 
    PRIMARY KEY (name, fulfiller_id, ext_ful_loc_id),
-   FOREIGN KEY (fulfiller_id, ext_ful_loc_id) REFERENCES Location
+   FOREIGN KEY (ext_ful_loc_id, fulfiller_id) REFERENCES Location(ext_ful_loc_id, fulfiller_id)
 );
 
-CREATE TABLE FulfillerSpecificProduct (
+CREATE TABLE IF NOT EXISTS FulfillerSpecificProduct (
    sku VARCHAR(100),
-   fulfiller_id VARCHAR(100),
+   fulfiller_id INT,
    upc VARCHAR(100),
 
    PRIMARY KEY (sku, fulfiller_id),
    UNIQUE (fulfiller_id, upc),
    FOREIGN KEY (fulfiller_id) REFERENCES Fulfiller(id),
-   FOREIGN KEY (upc) REFERENCES Product
+   FOREIGN KEY (upc) REFERENCES Product(upc)
 );
 
-CREATE TABLE HeldAt (
-   fulfiller_id VARCHAR(100),
-   ext_ful_loc_id INT,
+CREATE TABLE IF NOT EXISTS HeldAt (
+   fulfiller_id INT,
+   ext_ful_loc_id VARCHAR(100),
    sku VARCHAR(100),
    ltd FLOAT,
    safety_stock INT,
 
    PRIMARY KEY (fulfiller_id, ext_ful_loc_id, sku),
-   FOREIGN KEY (fulfiller_id, ext_ful_loc_id) REFERENCES Location,
-   FOREIGN KEY (sku) REFERENCES FulfillerSpecificProduct
+   FOREIGN KEY (ext_ful_loc_id, fulfiller_id) REFERENCES Location(ext_ful_loc_id, fulfiller_id),
+   FOREIGN KEY (sku) REFERENCES FulfillerSpecificProduct(sku)
 );
 
-CREATE TABLE StoredIn (
+CREATE TABLE IF NOT EXISTS StoredIn (
    sku VARCHAR(100),
-   fulfiller_id VARCHAR(100),
+   fulfiller_id INT,
    bin_name VARCHAR(100),
-   ext_ful_loc_id INT,
+   ext_ful_loc_id VARCHAR(100),
    on_hand INT,
    num_allocated INT DEFAULT 0,
 
    PRIMARY KEY (sku, fulfiller_id, bin_name, ext_ful_loc_id),
-   FOREIGN KEY (sku) REFERENCES FulfillerSpecificProduct,
+   FOREIGN KEY (sku) REFERENCES FulfillerSpecificProduct(sku),
    FOREIGN KEY (bin_name, fulfiller_id, ext_ful_loc_id) REFERENCES Bin (name, fulfiller_id, ext_ful_loc_id)
 );
