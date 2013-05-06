bab98d3bec7a2765b4432ebbe20309aee5449533
kestein <kestein@vogon.csc.calpoly.edu>
2013-05-06 13:30:22 -0700

    clear database script

---
+++ DB-clear.sql
@@ -0,0 +1,15 @@
+--Kevin Stein  kestein@calpoly.edu
+--Alex Spotnitz  aspotnitz@gmail.com
+--Stephen Calabrese  sccalabr@calpoly.edu
+--Alex Boltunov  aboltunov@gmail.com
+--Luke Larson  lplarson@calpoly.edu
+DELETE FROM TABLE SubscribesTo;
+DELETE FROM TABLE Catalogues;
+DELETE FROM TABLE Manufacturers;
+DELETE FROM TABLE StoredAt;
+DELETE FROM TABLE StoredIn;
+DELETE FROM TABLE Bins;
+DELETE FROM TABLE Items;
+DELETE FROM TABLE Locations;
+DELETE FROM TABLE FullfillerIds;
+DELETE FROM TABLE FullfilledBy;
