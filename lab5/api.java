32f8d219b9509951cec8ba617bcebf48f2a04dc4
George Reyes <zypher452@yahoo.com>
2013-05-07 10:48:28 -0700

    added new api.java file that contains the skeletons of api calls.

---
+++ lab5/api.java
@@ -0,0 +1,53 @@
+
+
+
+
+
+public class api {
+
+	public void createFulfiller ()
+	{
+		
+		
+		System.out.println("in createFulfiller!");
+		
+	}
+	
+	public void createFullfillmentLocation ()
+	{
+		
+		System.out.println("in create Fulfillment Location");
+		
+		
+	}
+	
+	public void createManufacturerCatalog ()
+	{
+		
+		System.out.println("in createManufacturer Catalog");
+		
+	}
+	
+	public void createBin ()
+	{
+	
+		System.out.println("createBin");
+	
+	}
+	
+	public void refreshInventory()
+	{
+		
+		System.out.println("refreshInventory");
+		
+	}
+	
+	
+	
+}
+
+
+
+
+
+
