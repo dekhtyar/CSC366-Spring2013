1044861732d9db3ee8ca7c3887b6444d27814464
Peter Faiman <pfaiman@calpoly.edu>
2013-05-07 19:56:22 -0700

    added clear.sqlz

---
+++ DB-clear.sql
@@ -0,0 +1,17 @@
+﻿-- Cait Rahm
+-- Peter Faiman
+-- Jessica Cosio
+-- Chase Ricketts
+
+-- PostgreSQL
+
+DELETE FROM BinProducts;
+DELETE FROM Bins;
+DELETE FROM LocationProducts;
+DELETE FROM FulfillerProducts;
+DELETE FROM Products;
+DELETE FROM LocationCatalogs;
+DELETE FROM Locations;
+DELETE FROM Catalogs;
+DELETE FROM Manufacturers;
+DELETE FROM Fulfillers;
