c794e5f64adc2aa3706f9dd71a29fd9027377fb0
Peter Faiman <pfaiman@calpoly.edu>
2013-05-09 10:34:08 -0700

    load the templates on module load

---
+++ src/apersci/output/templates.go
@@ -0,0 +1,25 @@
+package output
+
+import "html/template"
+
+var templates *template.Template
+
+var templateNames = [...]string{
+	"request-createBin.xml",
+	"request-createFulfiller.xml",
+	"request-createFulfillmentLocation.xml",
+	"request-refreshInventory.xml",
+	"response-createBin.xml",
+	"response-createFulfiller.xml",
+	"response-createFulfillmentLocation.xml",
+	"response-refreshInventory.xml",
+}
+
+func init() {
+	templateDir := "src/apersci/output/templates/" // TODO configurable
+	templatePaths := make([]string, len(templateNames))
+	for i, name := range templateNames {
+		templatePaths[i] = templateDir + name
+	}
+	templates = template.Must(template.ParseFiles(templatePaths...))
+}
