f288927b044138ab4f7216f0f7c8f78ca04a1b9f
jaldag <jchristophera@hotmail.com>
2013-05-08 21:49:55 -0600

    Creates some fulfillers

---
+++ SHOPATRON-build-fulfiller.sql
@@ -0,0 +1,4 @@
+INSERT INTO Fulfiller VALUES (1, 'Hello Kitty Store');
+INSERT INTO Fulfiller VALUES (2, 'Maruchan Store');
+INSERT INTO Fulfiller VALUES (3, 'Disney Princess Store');
+INSERT INTO Fulfiller VALUES (4, 'Barbie and Ken Store');
