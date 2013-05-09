aaff64d1fb1efb6ce8c32ea71f2c86249f6108de
jaldag <jchristophera@hotmail.com>
2013-05-08 21:49:34 -0600

    Creates some bins

---
+++ SHOPATRON-build-bins.sql
@@ -0,0 +1,4 @@
+INSERT INTO Bin VALUES (1, 1, 1, 'Small', 'Not Full', 'Hello Kitty Dolls');
+INSERT INTO Bin VALUES (2, 2, 2, 'Medium', 'Empty', 'Maruchan Dolls');
+INSERT INTO Bin VALUES (3, 3, 3, 'Large', 'Full', 'Snow White Dolls');
+INSERT INTO Bin VALUES (4, 4, 4, 'Huge', 'Empty', 'Barbie Dolls');
