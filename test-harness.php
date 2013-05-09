311a84c6d63f4bb5949d10f985060aadbf22576d
rossmckelvie <r@rossmckelvie.com>
2013-05-09 10:38:42 -0700

    test harness routing

--- test-harness.php
+++ test-harness.php
@@ -2,73 +2,90 @@
   include_once('db.php');
   include_once('db_functions.php');
 
-  $csv = array();
-  $csv['locations']           = 'data-csv-rfc/fulfiller_locations.csv';
-  $csv['location_bins']       = 'data-csv-rfc/fulfiller_location_bins.csv';
-  $csv['inv_available_bins']  = 'data-csv-rfc/fulfiller_inventory_available_bins.csv';
-  $csv['inv_available']       = 'data-csv-rfc/fulfiller_inventory_available.csv';
-  $csv['inv_not_available']   = 'data-csv-rfc/fulfiller_inventory_not_available.csv';
-
-  // Comma separated values
-  $delimiter = ',';
-
   // **********************************************************************
-  // Locations
+  // Route test harness
   // **********************************************************************
-  $handle = fopen($csv['locations'], "r");
-  if (!$handle) die();
+  switch ($argv[1]):
+    case 'seed':
+      db_seed();
+      break;
 
-  $header = fgetcsv($handle);
-  while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
-    print_r(array_combine($header, $line_array));
+    default:
+      print "Usage: php test-harness.php create|seed|empty|destroy\n";
+      break;
+
+  endswitch;
+
+  function db_seed() {
+    $csv = array();
+    $csv['locations']           = 'data-csv-rfc/fulfiller_locations.csv';
+    $csv['location_bins']       = 'data-csv-rfc/fulfiller_location_bins.csv';
+    $csv['inv_available_bins']  = 'data-csv-rfc/fulfiller_inventory_available_bins.csv';
+    $csv['inv_available']       = 'data-csv-rfc/fulfiller_inventory_available.csv';
+    $csv['inv_not_available']   = 'data-csv-rfc/fulfiller_inventory_not_available.csv';
+
+    // Comma separated values
+    $delimiter = ',';
+
+    // **********************************************************************
+    // Locations
+    // **********************************************************************
+    $handle = fopen($csv['locations'], "r");
+    if (!$handle) die();
+
+    $header = fgetcsv($handle);
+    while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
+      print_r(array_combine($header, $line_array));
+    }
+    fclose($handle);
+
+    // **********************************************************************
+    // Bins
+    // **********************************************************************
+    $handle = fopen($csv['location_bins'], "r");
+    if (!$handle) die();
+
+    $header = fgetcsv($handle);
+    while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
+      print_r(array_combine($header, $line_array));
+    }
+    fclose($handle);
+
+    // **********************************************************************
+    // Inventory Available Bins
+    // **********************************************************************
+    $handle = fopen($csv['inv_available_bins'], "r");
+    if (!$handle) die();
+
+    $header = fgetcsv($handle);
+    while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
+      print_r(array_combine($header, $line_array));
+    }
+    fclose($handle);
+
+    // **********************************************************************
+    // Inventory Available
+    // **********************************************************************
+    $handle = fopen($csv['inv_available'], "r");
+    if (!$handle) die();
+
+    $header = fgetcsv($handle);
+    while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
+      print_r(array_combine($header, $line_array));
+    }
+    fclose($handle);
+
+    // **********************************************************************
+    // Inventory Not Available
+    // **********************************************************************
+    $handle = fopen($csv['inv_not_available'], "r");
+    if (!$handle) die();
+
+    $header = fgetcsv($handle);
+    while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
+      print_r(array_combine($header, $line_array));
+    }
+    fclose($handle);
   }
-  fclose($handle);
 
-  // **********************************************************************
-  // Bins
-  // **********************************************************************
-  $handle = fopen($csv['location_bins'], "r");
-  if (!$handle) die();
-
-  $header = fgetcsv($handle);
-  while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
-    print_r(array_combine($header, $line_array));
-  }
-  fclose($handle);
-
-  // **********************************************************************
-  // Inventory Available Bins
-  // **********************************************************************
-  $handle = fopen($csv['inv_available_bins'], "r");
-  if (!$handle) die();
-
-  $header = fgetcsv($handle);
-  while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
-    print_r(array_combine($header, $line_array));
-  }
-  fclose($handle);
-
-  // **********************************************************************
-  // Inventory Available
-  // **********************************************************************
-  $handle = fopen($csv['inv_available'], "r");
-  if (!$handle) die();
-
-  $header = fgetcsv($handle);
-  while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
-    print_r(array_combine($header, $line_array));
-  }
-  fclose($handle);
-
-  // **********************************************************************
-  // Inventory Not Available
-  // **********************************************************************
-  $handle = fopen($csv['inv_not_available'], "r");
-  if (!$handle) die();
-
-  $header = fgetcsv($handle);
-  while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
-    print_r(array_combine($header, $line_array));
-  }
-  fclose($handle);
 ?>
