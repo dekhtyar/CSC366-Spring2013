89dc9c1aa9325be3dfc38f4d96d6c05401b2f330
jaldag <jchristophera@hotmail.com>
2013-05-08 21:50:39 -0600

    Creates some locations

---
+++ SHOPATRON-build-location.sql
@@ -0,0 +1,4 @@
+INSERT INTO Location VALUES (1, 1, 1, 'ABCD', 'SLO Hello Kitty Store', 'Mom n Pop Store', 1.0, 1.0, 1, 'SLO', 1, 1, 1);
+INSERT INTO Location VALUES (2, 2, 2, 'ABCDE', 'SLO Maruchan Store', 'Mom n Pop Store', 1.0, 1.0, 1, 'SLO', 1, 1, 1);
+INSERT INTO Location VALUES (3, 3, 3, 'ABCDEF', 'SLO Disney Princess Store', 'Mom n Pop Store', 1.0, 1.0, 1, 'SLO', 1, 1, 1);
+INSERT INTO Location VALUES (4, 4, 4, 'ABCDEFG', 'SLO Barbie and Ken Store', 'Mom n Pop Store', 1.0, 1.0, 1, 'SLO', 1, 1, 1);
