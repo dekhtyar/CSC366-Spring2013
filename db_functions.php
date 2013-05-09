38ad2251906e3aa18bc39d9a079500f415e30955
rossmckelvie <r@rossmckelvie.com>
2013-05-09 10:13:40 -0700

    remove outer function

--- db_functions.php
+++ db_functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once("db.php");
 
 function createFulfiller( $id ) {
@@ -7,13 +7,13 @@ function createFulfiller( $id ) {
   $stmt->execute();
 }
 
-function createFulfillmentLocation($locationId, $fulfillerId, $locationName, 
+function createFulfillmentLocation($locationId, $fulfillerId, $locationName,
   $locationType, $latitude, $longitude, $status, $safetyStock) {
     $stmt = $db->prepare("
-      INSERT INTO Fulfillers 
-      (locationId, fulfillerId, name, locationType, latitude, longitude, 
-      status, safetyStockLimitDefault) 
-      VALUES 
+      INSERT INTO Fulfillers
+      (locationId, fulfillerId, name, locationType, latitude, longitude,
+      status, safetyStockLimitDefault)
+      VALUES
       (:locationId, :fulfillerId, :name, :locationType, :latitude, :longitude,
       :status, :safetyStock)");
     $stmt->bindValue(':locationId',$locationId);
@@ -25,10 +25,10 @@ function createFulfillmentLocation($locationId, $fulfillerId, $locationName,
     $stmt->bindValue(':status',$status);
     $stmt->bindValue(':safeyStock',$safetyStock);
     $stmt->execute();
-  } 
+  }
 
 function createBin($binName, $fulfillerId, $locationId, $binType, $status)
-{ 
+{
   $stmt = $db->prepare("INSERT INTO Bins( BinName, FulfillerID, LocationID, binType, Status) VALUES (:BinName, :FulfillerID, :FulfillerLocationID, :BinTypeID, :BinStatusID)");
   $stmt->bindValue(':BinName', $binName);
   $stmt->bindValue(':FulfillerId', $fulfillerId);
@@ -39,40 +39,38 @@ function createBin($binName, $fulfillerId, $locationId, $binType, $status)
 }
 
 }
-function refreshInventory( $LocationName, $SKU, $UPC, $BinID, 
-  $Quantity, $LTD, $SafetyStock) {
-    function refreshInventory($fulfillerId, $locationId, $items) {
-      $item = {}; // ?
 
-      $stmt1 = $db->prepare("INSERT INTO BinContainsProducts(BinId, FulfillerId, LocationId, UPC, Allocated, OnHand) 
-        VALUES(:binId, :fulfillerId, :locationId, :UPC, :allocated, :onHand)");
-      $stmt1->bindParam(':binId', $item['BinId']);
-      $stmt1->bindParam(':fulfillerId', $item['FulfillerId']);
-      $stmt1->bindParam(':locationId', $item['LocationId']);
-      $stmt1->bindParam(':UPC', $item['UPC']);
-      $stmt1->bindParam(':allocated', $item['Allocated']);
-      $stmt1->bindParam(':onHand', $item['Onhand']);
+function refreshInventory($fulfillerId, $locationId, $items) {
+  $item = {}; // ?
 
-      $stmt2 = $db->prepare("INSERT INTO FulfillerCarriesProducts(FulfillerId, UPC, SKU) 
-        VALUES(:fulfillerId, :UPC, :SKU)");
-      $stmt2->bindParam(':fulfillerId', $item['FulfillerId']);
-      $stmt2->bindParam(':UPC', $item['UPC']);
-      $stmt2->bindParam(':SKU', $item['SKU']);
+  $stmt1 = $db->prepare("INSERT INTO BinContainsProducts(BinId, FulfillerId, LocationId, UPC, Allocated, OnHand)
+    VALUES(:binId, :fulfillerId, :locationId, :UPC, :allocated, :onHand)");
+  $stmt1->bindParam(':binId', $item['BinId']);
+  $stmt1->bindParam(':fulfillerId', $item['FulfillerId']);
+  $stmt1->bindParam(':locationId', $item['LocationId']);
+  $stmt1->bindParam(':UPC', $item['UPC']);
+  $stmt1->bindParam(':allocated', $item['Allocated']);
+  $stmt1->bindParam(':onHand', $item['Onhand']);
 
-      $stmt3 = $db->prepare("INSERT INTO LocationSellsProducts(FulfillerId, LocationId, UPC, SafetyStock, LTD)
-        VALUES(:fulfillerId, :locationId, :UPC, :safetyStock, :LTD)");
-      $stmt3->bindParam(':fulfillerId', $item['FulfillerId']);
-      $stmt3->bindParam(':locationId', $item['LocationId']);
-      $stmt3->bindParam(':UPC', $item['UPC']);
-      $stmt3->bindParam(':safetyStock', $item['SafetyStock']);
-      $stmt3->bindParam(':LTD', $item['LTD']);
+  $stmt2 = $db->prepare("INSERT INTO FulfillerCarriesProducts(FulfillerId, UPC, SKU)
+    VALUES(:fulfillerId, :UPC, :SKU)");
+  $stmt2->bindParam(':fulfillerId', $item['FulfillerId']);
+  $stmt2->bindParam(':UPC', $item['UPC']);
+  $stmt2->bindParam(':SKU', $item['SKU']);
 
-      foreach($items as $item) {
-        $stmt1->execute();
-        $stmt2->execute();
-        $stmt3->execute();
-      }
-    }
+  $stmt3 = $db->prepare("INSERT INTO LocationSellsProducts(FulfillerId, LocationId, UPC, SafetyStock, LTD)
+    VALUES(:fulfillerId, :locationId, :UPC, :safetyStock, :LTD)");
+  $stmt3->bindParam(':fulfillerId', $item['FulfillerId']);
+  $stmt3->bindParam(':locationId', $item['LocationId']);
+  $stmt3->bindParam(':UPC', $item['UPC']);
+  $stmt3->bindParam(':safetyStock', $item['SafetyStock']);
+  $stmt3->bindParam(':LTD', $item['LTD']);
+
+  foreach($items as $item) {
+    $stmt1->execute();
+    $stmt2->execute();
+    $stmt3->execute();
   }
+}
 ?>
 
