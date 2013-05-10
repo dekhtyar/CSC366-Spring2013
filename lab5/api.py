c10015a1c5575f84a94403bc8198fcf37e178a3d
Alex Boltunov <aboltunov@gmail.com>
2013-05-09 22:07:38 -0700

    Implement refreshInventory api call

--- lab5/api.py
+++ lab5/api.py
@@ -1,5 +1,7 @@
 # API functions
 
+import MySQLdb
+
 # Stephen
 def createFulfiller():
     pass
@@ -17,5 +19,43 @@ def createBin():
     pass
 
 # Alex B.
-def refreshInventory():
-    pass
+def refreshInventory(fulfiller_id, location_id, item):
+    sql_stored_at_where = 'WHERE UPC = %s AND FulfillerId = %s AND FulfillerLocationId = %s'
+    sql_stored_in_where = sql_stored_at_where+' AND Name = %s' 
+    args_stored_at_where = (item['upc'], fulfiller_id, location_id)
+    args_stored_in_where = args_stored_at_where + (item['binname'],)
+
+    con = MySQLdb.connect('localhost', 'root', 'busmajorz', 'inventory')
+    cur = con.cursor()
+
+    # Update 'Items' table
+    cur.execute('SELECT UPC FROM Items WHERE UPC = %s', (item['upc'],)) 
+    if not cur.fetchone():
+        cur.execute('INSERT INTO Items VALUES (%s, %s)',
+                    (item['upc'], item['name']))
+    else:
+        cur.execute('UPDATE Items SET Name = %s WHERE UPC = %s',
+                    (item['name'], item['upc']))
+
+    # Update 'StoredIn' table
+    cur.execute('SELECT UPC FROM StoredIn '+sql_stored_in_where,
+                args_stored_in_where)
+    if not cur.fetchone():
+        cur.execute('INSERT INTO StoredIn VALUES (%s, %s, %s, %s, %s, %s)',
+                    args_stored_in_where+(item['quantity'], 0))
+    else:
+        cur.execute('UPDATE StoredIn SET OnHand = %s ' + sql_stored_in_where,
+                    (item['quantity'],)+args_stored_in_where)
+
+    # Update 'StoredAt' table
+    cur.execute('SELECT UPC FROM StoredAt '+sql_stored_at_where,
+                args_stored_at_where)
+    if not cur.fetchone():
+        cur.execute('INSERT INTO StoredAt VALUES (%s, %s, %s, %s, %s)',
+                    args_stored_at_where+(item['ltd'], item['safety']))
+    else:
+        cur.execute('UPDATE StoredAt SET LTD = %s, SafetyStockLimit = %s '+
+                    sql_stored_at_where,
+                    (item['ltd'], item['safety'])+args_stored_at_where)
+
+    con.close()
