d3cfd41dc2c1ad93c4168b6b8a919cf432c7840d
Peter Faiman <pfaiman@calpoly.edu>
2013-05-09 20:55:15 -0700

    added test cases

--- src/apersci/output/output_test.go
+++ src/apersci/output/output_test.go
@@ -1,12 +1,119 @@
 package output
 
 import (
+	"apersci/soap"
 	"testing"
+
 //	"log"
 //	"bytes"
 )
 
-const buffer_size = 4096
+var createBinRequestTests = []struct {
+	Input  soap.Bin
+	Output string
+}{
+	{
+		Input: soap.Bin{42, 43, 44, "abc", "def", "ghi"},
+		Output: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v4="http://v4.core.coexprivate.api.shopatron.com">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <v4:createBin>
+         <v4:request>
+            <v4:FulfillerID>42</v4:FulfillerID>
+            <v4:BinID>43</v4:BinID>
+            <v4:FulfillerLocationID>44</v4:FulfillerLocationID>
+            <v4:BinType>abc</v4:BinType>
+            <v4:BinStatus>def</v4:BinStatus>
+            <v4:Name>ghi</v4:Name>
+         </v4:request>
+      </v4:createBin>
+   </soapenv:Body>
+</soapenv:Envelope>
+`},
+}
+
+var createBinResponseTests = []struct {
+	Input  string
+	Output string
+}{
+	{
+		Input: "abc",
+		Output: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v4="http://v4.core.coexprivate.api.shopatron.com">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <v4:createBinResponse>
+          <v4:createBinReturn>abc</v4:createBinReturn>
+      </v4:createBinResponse>
+   </soapenv:Body>
+</soapenv:Envelope>
+`},
+}
+
+var createFulfillerRequestTests = []struct {
+	Input  soap.FulfillerRequest
+	Output string
+}{
+	{
+		Input: soap.FulfillerRequest{42, "abc"},
+		Output: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v4="http://v4.core.coexprivate.api.shopatron.com">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <v4:createFulfiller>
+         <v4:request>
+            <v4:FulfillerID>42</v4:FulfillerID>
+            <v4:Name>abc</v4:Name>
+         </v4:request>
+      </v4:createFulfiller>
+   </soapenv:Body>
+</soapenv:Envelope>
+`},
+}
+
+var createFulfillerResponseTests = []struct {
+	Input  string
+	Output string
+}{
+	{
+		Input: "abc",
+		Output: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v4="http://v4.core.coexprivate.api.shopatron.com">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <v4:createFulfillerResponse>
+          <v4:createFulfillerReturn>abc</v4:createFulfillerReturn>
+      </v4:createFulfillerResponse>
+   </soapenv:Body>
+</soapenv:Envelope>
+`},
+}
+
+var createFulfillmentLocationTests = []struct {
+	Input  soap.FulfillmentLocation
+	Output string
+}{
+	{
+		Input: soap.FulfillmentLocation{42, 43, 44, "abc", "def", "ghi", 1.234, 5.678, 45, "JK"},
+		Output: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:v4="http://v4.core.coexprivate.api.shopatron.com">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <v4:createFulfillmentLocation>
+         <v4:request>
+            <v4:FulfillerID>42</v4:FulfillerID>
+            <v4:RetailerLocationID>43</v4:RetailerLocationID>
+            <v4:ExternalLocationID>44</v4:ExternalLocationID>
+            <v4:LocationName>abc</v4:LocationName>
+            <v4:LocationType>def</v4:LocationType>
+            <v4:Latitude>ghi</v4:Latitude>
+            <v4:Longitude>1.234</v4:Longitude>
+            <v4:Status>5.678</v4:Status>
+            <v4:CountryCode>JK</v4:CountryCode>
+         </v4:request>
+      </v4:createFulfillmentLocation>
+   </soapenv:Body>
+</soapenv:Envelope>
+`},
+}
+
+const bufferSize = 4096
 
 /*
 
@@ -22,8 +129,8 @@ func CreateBinResponse(w io.Writer, v string) error {
 */
 
 func errorMismatch(expOutput string, actOutput string) {
-	t.ErrorF("Expected output did not match the actual output.\n" + 
-			 "Expected:\n" + expOutput + "\nActual:\n" + actOutput)
+	t.ErrorF("Expected output did not match the actual output.\n" +
+		"Expected:\n" + expOutput + "\nActual:\n" + actOutput)
 }
 
 // TODO merge code in functions below
@@ -31,7 +138,7 @@ func errorMismatch(expOutput string, actOutput string) {
 func TestCreateBinRequests(t *testing.T) {
 	for _, strct := range createBinRequestTests {
 		input := strct.Input
-		actOutput []byte := make([]byte, 0, buffer_size)
+		actOutput := make([]byte, 0, bufferSize)
 		w := bytes.NewBuffer(actOutput)
 
 		err := CreateBinRequest(w, input)
@@ -49,8 +156,7 @@ func TestCreateBinResponse(t *testing.T) {
 	for _, strct := range createBinResponseTests {
 		input := strct.Input
 
-
-		actOutput []byte := make([]byte, 0, buffer_size)
+		actOutput := make([]byte, 0, bufferSize)
 		w := bytes.NewBuffer(actOutput)
 
 		err := CreateBinRequest(w, input)
@@ -76,4 +182,3 @@ func createBin() &soap.Bin {
 	return bin
 }
 */
-
