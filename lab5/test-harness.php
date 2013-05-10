f8ed49cf9964311257163fbe6b57bd5cc3fbeba0
rossmckelvie <r@rossmckelvie.com>
2013-05-09 23:27:39 -0700

    ids varchar(10) to (11)

--- lab5/test-harness.php
+++ lab5/test-harness.php
@@ -41,15 +41,15 @@ function db_create_tables($db) {
   try {
     $db->exec("
       create table Catalogs (
-        catalogId VARCHAR(10) NOT NULL,
-        manufacturerId VARCHAR(10) NOT NULL,
+        catalogId VARCHAR(11) NOT NULL,
+        manufacturerId VARCHAR(11) NOT NULL,
         constraint catalogs_pk PRIMARY KEY (catalogId, manufacturerId)
       );
     ");
 
     $db->exec("
       create table Fulfillers (
-        fulfillerId VARCHAR(10) NOT NULL,
+        fulfillerId VARCHAR(11) NOT NULL,
         name VARCHAR(32) NOT NULL,
         constraint fulfillers_pk PRIMARY KEY (fulfillerId)
       );
@@ -57,9 +57,9 @@ function db_create_tables($db) {
 
     $db->exec("
       create table Locations (
-        externalLocationId VARCHAR(10) NOT NULL,
-        internalLocationId VARCHAR(32) NOT NULL,
-        fulfillerId VARCHAR(10) NOT NULL,
+        externalLocationId VARCHAR(11) NOT NULL,
+        internalLocationId VARCHAR(11) NOT NULL,
+        fulfillerId VARCHAR(11) NOT NULL,
         locationType VARCHAR(50),
         latitude decimal(9,6),
         longitude decimal(9,6),
@@ -83,8 +83,8 @@ function db_create_tables($db) {
 
     $db->exec("
       create table Bins (
-        locationId VARCHAR(10) NOT NULL,
-        fulfillerId VARCHAR(10) NOT NULL,
+        locationId VARCHAR(11) NOT NULL,
+        fulfillerId VARCHAR(11) NOT NULL,
         binName VARCHAR(50) NOT NULL,
         binType VARCHAR(50),
         status VARCHAR(10),
@@ -95,10 +95,10 @@ function db_create_tables($db) {
 
     $db->exec("
       create table LocationOffersCatalogs (
-        catalogId VARCHAR(10) NOT NULL,
-        manufacturerId VARCHAR(10) NOT NULL,
-        locationId VARCHAR(10) NOT NULL,
-        fulfillerId VARCHAR(10),
+        catalogId VARCHAR(11) NOT NULL,
+        manufacturerId VARCHAR(11) NOT NULL,
+        locationId VARCHAR(11) NOT NULL,
+        fulfillerId VARCHAR(11),
         constraint loc_catalog_fk FOREIGN KEY (catalogId, manufacturerId) REFERENCES Catalogs (catalogId, manufacturerId),
         constraint loc_location_fk FOREIGN KEY (locationId) REFERENCES Locations (internalLocationId),
         constraint loc_pk PRIMARY KEY (catalogId, manufacturerId, locationId, fulfillerId)
@@ -107,9 +107,9 @@ function db_create_tables($db) {
 
     $db->exec("
       create table FulfillerCarriesProducts (
-        fulfillerId VARCHAR(10) NOT NULL,
-        productUpc VARCHAR(10) NOT NULL,
-        sku VARCHAR(10) NOT NULL,
+        fulfillerId VARCHAR(11) NOT NULL,
+        productUpc VARCHAR(11) NOT NULL,
+        sku VARCHAR(11) NOT NULL,
         constraint fcp_fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers (fulfillerId),
         constraint fcp_productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products (upc),
         constraint fcp_pk PRIMARY KEY (fulfillerId, productUpc)
@@ -118,12 +118,12 @@ function db_create_tables($db) {
 
     $db->exec("
       create table BinContainsProducts (
-        productUpc VARCHAR(10) NOT NULL,
-        binName VARCHAR(10) NOT NULL,
-        internalLocationId VARCHAR(10) NOT NULL,
+        productUpc VARCHAR(11) NOT NULL,
+        binName VARCHAR(11) NOT NULL,
+        internalLocationId VARCHAR(11) NOT NULL,
         allocated VARCHAR(6) DEFAULT '0',
         onHand VARCHAR(6),
-        fulfillerId VARCHAR(10) NOT NULL,
+        fulfillerId VARCHAR(11) NOT NULL,
         constraint bcp_productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products (upc),
         constraint bcp_binname_fk FOREIGN KEY (internalLocationId, fulfillerId, binName) REFERENCES Bins (locationId, fulfillerId, binName),
         constraint bcp_location_fk FOREIGN KEY (internalLocationId) REFERENCES Locations (internalLocationId),
