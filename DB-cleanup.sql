008cfc04465d30010a54f536d4c5219126a1e41a
Alex Boltunov <aboltunov@gmail.com>
2013-05-09 22:46:43 -0700

    Update sql scripts to work with MySQL

--- DB-cleanup.sql
+++ DB-cleanup.sql
@@ -1,17 +1,17 @@
---Kevin Stein  kestein@calpoly.edu
---Alex Spotnitz  aspotnitz@gmail.com
---Stephen Calabrese  sccalabr@calpoly.edu
---Alex Boltunov  aboltunov@gmail.com
---Luke Larson  lplarson@calpoly.edu
+-- Kevin Stein  kestein@calpoly.edu
+-- Alex Spotnitz  aspotnitz@gmail.com
+-- Stephen Calabrese  sccalabr@calpoly.edu
+-- Alex Boltunov  aboltunov@gmail.com
+-- Luke Larson  lplarson@calpoly.edu
 
-DROP TABLE SubscribeTo CASCADE CONSTRAINTS;
-DROP TABLE Catalogues CASCADE CONSTRAINTS;
-DROP TABLE Manufacturers CASCADE CONSTRAINTS;
-DROP TABLE StoredAt CASCADE CONSTRAINTS;
-DROP TABLE StoredIn CASCADE CONSTRAINTS;
-DROP TABLE Bins CASCADE CONSTRAINTS;
-DROP TABLE Items CASCADE CONSTRAINTS;
-DROP TABLE Locations CASCADE CONSTRAINTS;
-DROP TABLE Fulfillers CASCADE CONSTRAINTS;
-DROP TABLE FulfilledBy CASCADE CONSTRAINTS;
-DROP TABLE ListedIn CASCADE CONSTRAINTS;
+DROP TABLE SubscribeTo CASCADE;
+DROP TABLE Catalogues CASCADE;
+DROP TABLE Manufacturers CASCADE;
+DROP TABLE StoredAt CASCADE;
+DROP TABLE StoredIn CASCADE;
+DROP TABLE Bins CASCADE;
+DROP TABLE Items CASCADE;
+DROP TABLE Locations CASCADE;
+DROP TABLE Fulfillers CASCADE;
+DROP TABLE FulfilledBy CASCADE;
+DROP TABLE ListedIn CASCADE;
