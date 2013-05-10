9c26a93749ad9cf8c998e7ed893766d4c0108b54
rossmckelvie <r@rossmckelvie.com>
2013-05-10 00:07:11 -0700

    remove unused variable

--- lab5/test-harness.php
+++ lab5/test-harness.php
@@ -256,7 +256,6 @@ function db_seed($db) {
   $data[] = get_csv_data($csv['inv_available_bins']);
   $data[] = get_csv_data($csv['inv_available']);
   $data[] = get_csv_data($csv['inv_not_available']);
-  $i = 0;
 
   foreach($data as &$data_chunk)
     $api->refreshInventory($data_chunk);
