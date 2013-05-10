b2dd577dce06fa0f87b35d51ddfbd6bd575aa1d6
Peter Faiman <pfaiman@calpoly.edu>
2013-05-09 23:01:42 -0700

    fixed a problem with refreshInventory template

--- src/apersci/service/refreshInventory.go
+++ src/apersci/service/refreshInventory.go
@@ -11,5 +11,5 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 	data, err := input.RefreshInventoryRequest(r.Body)
 	fmt.Println(err)
 	fmt.Println(data)
-	output.RefreshInventoryResponse(w, ":-D")
+	output.RefreshInventoryResponse(w, "Inventory Updated yo!")
 }
