46bea8b0b8acb107f42f1410743475a850c3ba6c
Peter Faiman <pfaiman@calpoly.edu>
2013-05-09 22:50:05 -0700

    client functions for lab 5

---
+++ src/apersci/client/client.go
@@ -0,0 +1,69 @@
+package client
+
+import (
+	"apersci/input"
+	"apersci/output"
+	"apersci/soap"
+	"bytes"
+	"net/http"
+)
+
+func CreateBin(url string, v soap.Bin) (r string, err error) {
+	var b bytes.Buffer
+
+	err = output.CreateBinRequest(&b, v)
+	if err != nil {
+		return
+	}
+	resp, err := http.Post(url+"/createBin/", "application/soap+xml", &b)
+	if err != nil {
+		return
+	}
+	r, err = input.CreateBinResponse(resp.Body)
+	return
+}
+
+func CreateFulfiller(url string, v soap.FulfillerRequest) (r string, err error) {
+	var b bytes.Buffer
+
+	err = output.CreateFulfillerRequest(&b, v)
+	if err != nil {
+		return
+	}
+	resp, err := http.Post(url+"/createFulfiller/", "application/soap+xml", &b)
+	if err != nil {
+		return
+	}
+	r, err = input.CreateFulfillerResponse(resp.Body)
+	return
+}
+
+func CreateFulfillmentLocation(url string, v soap.FulfillmentLocation) (r string, err error) {
+	var b bytes.Buffer
+
+	err = output.CreateFulfillmentLocationRequest(&b, v)
+	if err != nil {
+		return
+	}
+	resp, err := http.Post(url+"/createFulfillmentLocation/", "application/soap+xml", &b)
+	if err != nil {
+		return
+	}
+	r, err = input.CreateFulfillmentLocationResponse(resp.Body)
+	return
+}
+
+func RefreshInventory(url string, v soap.RefreshRequest) (r string, err error) {
+	var b bytes.Buffer
+
+	err = output.RefreshInventoryRequest(&b, v)
+	if err != nil {
+		return
+	}
+	resp, err := http.Post(url+"/refreshInventory/", "application/soap+xml", &b)
+	if err != nil {
+		return
+	}
+	r, err = input.RefreshInventoryResponse(resp.Body)
+	return
+}
