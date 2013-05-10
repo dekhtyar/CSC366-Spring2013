a07f2abac7ad716b3811bd40f114a7bdd1522583
dtorre08 <dtorre08@calpoly.edu>
2013-05-09 21:44:34 -0700

    Made final changes

--- lab5/api.java
+++ lab5/api.java
@@ -6,6 +6,30 @@ import java.lang.*;
 public class api {
    // The Connection var needed to connect to the database
    private Connection conn;
+   private ArrayList<Integer> ids = new ArrayList<Integer>();
+   private ArrayList<Integer> mIds = new ArrayList<Integer>();
+   private ArrayList<Integer> cIds = new ArrayList<Integer>();
+   private ArrayList<Integer> intFulLocIds = new ArrayList<Integer>();
+
+   public class RefreshItem {
+        String partNumber;
+        String UPC;
+        int BinID;
+        int Quantity;
+        double LTD;
+        int SafetyStock;
+      
+        public RefreshItem(String partNumber, String UPC, int BinID,
+           int Quantity, double LTD, int SafetyStock)
+         {
+           this.partNumber = partNumber;
+           this.UPC = UPC;
+           this.BinID = BinID;
+           this.Quantity = Quantity;
+           this.LTD = LTD;
+           this.SafetyStock = SafetyStock;
+         }
+    }
 
    public boolean setUpConnection()
    {
@@ -23,7 +47,7 @@ public class api {
 
       try
       {
-         conn = DriverManager.getConnection("jdbc:mysql:@cslvm54.csc.calpoly.edu", "amille35", "dedb59bc");
+         conn = DriverManager.getConnection("jdbc:mysql://localhost/glade", "root", "");
       }
       catch(Exception e)
       {
@@ -55,20 +79,47 @@ public class api {
 
    public void createFulfillmentLocation (String locationName, int fulfillerId,
       String externalLocationId, int internalFulfillerLocationId,
-      String description, double latitude, double longitude, int status,
+      String description, double latitude, double longitude, String status,
       int safetyStockLimit, int manufacturerId, int catalogId)
    {
       if(setUpConnection() == false)
          return;
 
+      //if(closeConnection() == false)
+         //return;
+
+      if(!ids.contains(new Integer(fulfillerId))) {
+         createNewRetailer(fulfillerId, locationName);
+         ids.add(new Integer(fulfillerId));
+      }
+
+      if(!mIds.contains(new Integer(manufacturerId))) {
+         createNewManufacturer(manufacturerId);
+         mIds.add(new Integer(manufacturerId));
+      }
+
+      if(!cIds.contains(new Integer(catalogId))) {
+         createNewCatalog(manufacturerId, catalogId);
+         cIds.add(new Integer(catalogId));
+      }
+      
+      if(!intFulLocIds.contains(new Integer(internalFulfillerLocationId))) {
+         createNewLocation(fulfillerId, externalLocationId, internalFulfillerLocationId,
+            "", description, latitude, longitude, status, safetyStockLimit);
+         intFulLocIds.add(new Integer(internalFulfillerLocationId));
+      }
+
+      //createNewRetailer(fulfillerId, locationName);
+      //createNewLocation(fulfillerId, externalLocationId, internalFulfillerLocationId,
+         //"", description, latitude, longitude, status, safetyStockLimit);
+      //createNewManufacturer(manufacturerId);
+      //createNewCatalog(manufacturerId, catalogId);
+      
+      createNewCatalogServedByLocation(internalFulfillerLocationId, manufacturerId,
+         catalogId);
+      
       if(closeConnection() == false)
          return;
-
-      createNewRetailer(fulfillerId, locationName);
-      createNewLocation(fulfillerId, externalLocationId, internalFulfillerLocationId,
-         "", description, latitude, longitude, status, safetyStockLimit);
-      createNewManufacturer(manufacturerId);
-      createNewCatalog(manufacturerId, catalogId);
    }
 
    public void createNewRetailer(int fulfillerId, String locationName)
@@ -86,13 +137,13 @@ public class api {
       }
       catch(Exception e)
       {
-         System.out.println("Error occured while creating a new Retailer tuple: ");
+         System.out.println("Error occured while creating a new Retailer tuple: " + e);
       }
    }
 
    public void createNewLocation(int fulfillerId, String externalLocationId,
       int internalFulfillerLocationId, String type, String description,
-      double latitude, double longitude, int status, int safetyStockLimit)
+      double latitude, double longitude, String status, int safetyStockLimit)
    {
        String sql = "INSERT INTO Location VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -107,14 +158,35 @@ public class api {
          ps.setString(5, description);
          ps.setDouble(6, latitude);
          ps.setDouble(7, longitude);
-         ps.setInt(8, status);
+         ps.setString(8, status);
          ps.setInt(9, safetyStockLimit);
 
          ps.executeUpdate();
       }
       catch(Exception e)
       {
-         System.out.println("Error occured while creating a new Location tuple: ");
+         System.out.println("Error occured while creating a new Location tuple: " + e);
+      }
+   }
+   
+   public void createNewCatalogServedByLocation(int internalFulfillerLocationId,
+      int manufacturerId, int catalogId)
+   {
+      String sql = "INSERT INTO CatalogServedByLocation (InternalFulfillerLocationId, ManufacturerId, CatalogId) VALUES(?, ?, ?)";
+
+      try
+      {
+         PreparedStatement ps = conn.prepareStatement(sql);
+
+         ps.setInt(1, internalFulfillerLocationId);
+         ps.setInt(2, manufacturerId);
+         ps.setInt(3, catalogId);
+
+         ps.executeUpdate();
+      }
+      catch(Exception e)
+      {
+         System.out.println("Error occured while creating a new CatalogServedByLocation tuple: " + e);
       }
    }
 
@@ -132,13 +204,13 @@ public class api {
       }
       catch(Exception e)
       {
-         System.out.println("Error occured while creating a new Manufacturer tuple: ");
+         System.out.println("Error occured while creating a new Manufacturer tuple: " + e);
       }
    }
 
    public void createNewCatalog(int manufacturerId, int catalogId)
    {
-      String sql = "INSERT INTO Catlog VALUES(?, ?)";
+      String sql = "INSERT INTO Catalog VALUES(?, ?)";
 
       try
       {
@@ -151,7 +223,7 @@ public class api {
       }
       catch(Exception e)
       {
-         System.out.println("Error occured while creating a new Catalog tuple: ");
+         System.out.println("Error occured while creating a new Catalog tuple: " + e);
       }
    }
 	
@@ -195,12 +267,105 @@ public class api {
       return 1;
    }
 	
-   public void refreshInventory()
-   {
-      System.out.println("refreshInventory");		
-   }	
-}
+   public int refreshInventory(int internalFulfillerLocationId, String LocationName, String SKU, String UPC,
+      int binId, int onhand, double ltd, int safetyStock)
+    {
+        int i = 0;
+        int InternalFulfillerId = -1;
+        int FulfillerId = -1;
+        String InternalFulfillerInt;
+        String FulfillerInt;
+        PreparedStatement s1;
+        PreparedStatement s2;
+        ResultSet InternalFulfillerHolder;
+        ResultSet FulfillerHolder;
 
+	setUpConnection();        
+
+        RefreshItem[] item = new RefreshItem[1];
+        item[0] = new RefreshItem(SKU, UPC, 0, onhand, ltd, safetyStock);
+        
+        if (FulfillerId < 0)
+            return -1;
+        
+        try {
+            FulfillerInt = "SELECT R.FulfillerId" +
+            "FROM LocationProduct LP, RetailerProduct RP,Retailer R, Location L"+
+            "where LP.RetailerProductId = RP.Id AND RP.FulfillerId = R.FulfillerId"+
+            "AND R.FulfillerId = L.FulfillerId";
+           
+           InternalFulfillerInt = "SELECT L.InternalFulfillerLocationId" +
+            "FROM LocationProduct LP, RetailerProduct RP,Retailer R, Location L"+
+            "where LP.InternalFulfillerLocationId = ?";
+        
+            s1 = conn.prepareStatement(FulfillerInt);
+            FulfillerHolder = s1.executeQuery();
+           
+            s2 = conn.prepareStatement(InternalFulfillerInt);
+            s2.setInt(1, internalFulfillerLocationId);
+            InternalFulfillerHolder = s2.executeQuery();
+        
+            InternalFulfillerId = InternalFulfillerHolder.getInt(0);
+            FulfillerId = FulfillerHolder.getInt(0);
+        }
+        catch (Exception e) {
+            System.out.println("Errors");
+        }
+        
+        for (i = 0; i < item.length; i++) {
+            if (InternalFulfillerId == -1) { // not there, so use INSERT
+                try {
+                    String RP = "INSERT INTO RetailerProduct (FulfillerId, UPC) VALUES(?, ?)"; //, SKU) VALUES(?, ?, ?)";
+                    String LP = "INSERT INTO LocationProduct (InternalFulfillerLocationId, LTD, SafeStockLimit) VALUES(?, ?, ?)";
+                    String CIB = "INSERT INTO ContainedInBin (BinId, OnHand, Allocated) VALUES(?, ?, ?)";
+                
+                    PreparedStatement ps1 = conn.prepareStatement(RP);
+                    ps1.setInt(1, FulfillerId);
+                    ps1.setString(2, item[i].UPC);
+                  //  ps1.setString(3, 0);
+                
+                    PreparedStatement ps2 = conn.prepareStatement(LP);
+                    ps2.setInt(1, InternalFulfillerId);
+                    ps2.setDouble(2, item[i].LTD);
+                    ps2.setInt(3, item[i].SafetyStock);
+                
+                    PreparedStatement ps3 = conn.prepareStatement(CIB);
+                    ps3.setInt(1, item[i].BinID);
+                    ps3.setInt(2, item[i].Quantity);
+                    ps3.setInt(3, 0);
+                
+                    ps1.executeUpdate();
+                    ps2.executeUpdate();
+                    ps3.executeUpdate();
+                }
+                catch (Exception e) {
+                    System.out.println("Can't insert for refresh inventory");
+                }
+            }
+            else { // use UPDATE statements
+                String sql = "UPDATE LocationProduct LP, RetailerProduct RP, ContainedInBin CB "+
+                "SET LP.LTD = ? AND LP.SafeStockLimit = ? AND CB.BinId = ? AND RP.UPC = ? AND CB.OnHand = ? " +
+                "WHERE LP.RetailerProductId = RP.Id AND CB.LocationProductId = LP.Id";
+                
+                try {
+                    PreparedStatement ps = conn.prepareStatement(sql);
+                    ps.setDouble(1, item[i].LTD);
+                    ps.setInt(2, item[i].SafetyStock);
+                    ps.setInt(3, item[i].BinID);
+                    ps.setString(4, item[i].UPC);
+                    ps.setInt(5, item[i].Quantity);
+                }
+                catch (Exception e) {
+                    System.out.println("Can't update for refresh inventory");
+                }
+                
+            }
+        }
+        
+        closeConnection(); 
+        return 1;
+    } 
+}
 
 
 
