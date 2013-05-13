4778d42d5917ba19343e3a5f42b0bdb5b9c2b1d2
Peter Faiman <pfaiman@calpoly.edu>
2013-05-13 05:01:59 -0700

    theoretically added execution of sql files

--- src/apersci/service/main.go
+++ src/apersci/service/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"database/sql"
+	"fmt"
 	_ "github.com/bmizerany/pq"
 	"net/http"
+	"os"
 )
+
+func getFileContents(fileName string) (contents string, err error) {
+	var b bytes.Buffer
+	f, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	_, err = b.ReadFrom(f)
+	if err != nil {
+		return
+	}
+	contents = b.String()
+	return
+}
+
+func execFile(fileName string) func(http.ResponseWriter, *http.Request) {
+	fileContents, err := getFileContents(fileName)
+	if err != nil {
+		panic(fmt.Sprintf("error reading SQL file (%s): %s\n", fileName, err.Error()))
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		conn, err := sql.Open("postgres", "dbname=db2 password=db2")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		defer conn.Close()
+
+		result, err := conn.Exec(fileContents)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
 
 func main() {
 	http.HandleFunc("/createBin/", createBin)
