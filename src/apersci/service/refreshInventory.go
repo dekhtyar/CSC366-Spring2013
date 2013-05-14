236472c9dda9455afd1d3412957e6737c67f2657
Peter Faiman <pfaiman@calpoly.edu>
2013-05-14 10:09:17 -0700

    fix refreshInventory a little

--- src/apersci/service/refreshInventory.go
+++ src/apersci/service/refreshInventory.go
@@ -11,11 +11,13 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 	rr, err := input.RefreshInventoryRequest(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	conn, err := getDBConnection()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	defer conn.Close()
 
@@ -23,13 +25,15 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 		tx, err := conn.Begin()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		defer tx.Rollback()
 
-		rows, err := tx.Query("SELECT COUNT(*) FROM BinsProducts WHERE binId = $1 AND sku = $2",
+		rows, err := tx.Query("SELECT COUNT(*) FROM BinProducts WHERE binId = $1 AND sku = $2",
 			i.BinID, i.PartNumber)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
 		var count uint
@@ -38,14 +42,16 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 		err = rows.Scan(&count)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		rows.Close()
 
 		if count > 0 {
-			_, err = tx.Exec("UPDATE BinsProducts SET onhandinventory = $1 WHERE sku = $2 AND binId = $3",
+			_, err = tx.Exec("UPDATE BinProducts SET onhandinventory = $1 WHERE sku = $2 AND binId = $3",
 				i.Quantity, i.PartNumber, i.BinID)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 		} else {
 			rows, err = tx.Query("SELECT COUNT(*) FROM Products WHERE upc = $1", i.UPC)
@@ -53,6 +59,7 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 			err = rows.Scan(&count)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 			rows.Close()
 
@@ -60,16 +67,18 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 				_, err = tx.Exec("INSERT INTO Products(upc) VALUES($1)", i.UPC)
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
 				}
 			}
-			_, err = tx.Exec("INSERT INTO BinsProducts VALUES($1, $2, $3, $4, 0)", i.BinID, rr.FulfillerID, i.PartNumber, i.Quantity)
+			_, err = tx.Exec("INSERT INTO BinProducts VALUES($1, $2, $3, $4, 0)", i.BinID, rr.FulfillerID, i.PartNumber, i.Quantity)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 		}
 
 		rows, err = tx.Query(`SELECT DISTINCT b.locationid
-							  FROM BinsProducts bp, Bins b WHERE bp.sku = $1 AND
+							  FROM BinProducts bp, Bins b WHERE bp.sku = $1 AND
 				              bp.binID = $2 AND bp.binID = b.id`, i.PartNumber, i.BinID)
 		var locationid uint
 
@@ -77,41 +86,47 @@ func refreshInventory(w http.ResponseWriter, r *http.Request) {
 		err = rows.Scan(&locationid)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		rows.Close()
 
-		rows, err = tx.Query(`SELECT COUNT(*) FROM LocationsProducts
+		rows, err = tx.Query(`SELECT COUNT(*) FROM LocationProducts
 							  WHERE locationid = $2
 							  AND sku = $1`, i.PartNumber, locationid)
 		rows.Next()
 		err = rows.Scan(&count)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		rows.Close()
 
 		if count == 0 {
-			_, err = tx.Exec("INSERT INTO LocationsProducts VALUES($1, $2, $3, $4, $5)",
+			_, err = tx.Exec("INSERT INTO LocationProducts VALUES($1, $2, $3, $4, $5)",
 				locationid, rr.FulfillerID, i.PartNumber, i.LTD, i.SafetyStock)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 		} else {
-			_, err = tx.Exec(`UPDATE LocationsProducts SET ltd = $1, safetyStock = $2 WHERE locationid = $4 AND sku = $3`,
+			_, err = tx.Exec(`UPDATE LocationProducts SET ltd = $1, safetyStock = $2 WHERE locationid = $4 AND sku = $3`,
 				i.LTD, i.SafetyStock, i.PartNumber, locationid)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 		}
 
 		err = tx.Commit()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	}
 
 	err = output.RefreshInventoryResponse(w, fmt.Sprint(len(rr.Items)))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
