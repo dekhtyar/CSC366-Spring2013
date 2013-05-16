6963777cafac06b4e0de378e61dbf3b980cccf71
rossmckelvie <r@rossmckelvie.com>
2013-05-15 19:24:29 -0700

    setup projects folder

---
+++ project/team-ross-api.php
@@ -0,0 +1,207 @@
+<?php
+error_reporting(E_ERROR);
+
+class TeamRossAPI {
+  private $db;
+
+  public function __construct($db_handle) {
+    $this->db = $db_handle;
+  }
+
+  public function createFulfiller( $id, $name ) {
+    $stmt = $this->db->prepare("INSERT INTO Fulfillers (fulfillerId, name) VALUES (:id,:name)");
+    $stmt->bindValue(':id', $id);
+    $stmt->bindValue(':name',$name);
+    $stmt->execute();
+  }
+
+  public function createFulfillmentLocation($locationName, $extLID, $intLID,
+    $fulfillerId, $locationType, $latitude, $longitude, $status, $safetyStock, $mfgId, $catalogId) {
+      // Create Location
+      $stmt = $this->db->prepare("
+        INSERT INTO Locations
+          (externalLocationId, internalLocationId, fulfillerId, locationType,
+          latitude, longitude, status, safetyStockLimitDefault)
+        VALUES
+          (:externalLocationId, :internalLocationId, :fulfillerId, :locationType,
+           :latitude, :longitude, :status, :safetyStockLimitDefault);
+      ");
+
+      $stmt->bindValue(':externalLocationId', $extLID);
+      $stmt->bindValue(':internalLocationId', $intLID);
+      $stmt->bindValue(':fulfillerId', $fulfillerId);
+      $stmt->bindValue(':locationType', $locationType);
+      $stmt->bindValue(':latitude', strval($latitude));
+      $stmt->bindValue(':longitude', strval($longitude));
+      $stmt->bindValue(':status', $status);
+      $stmt->bindValue(':safetyStockLimitDefault', $safetyStock);
+
+      $stmt->execute();
+
+      // Create LocationOffersCatalog
+      $relational = $this->db->prepare("
+	SET foreign_key_checks=0;
+        INSERT INTO LocationOffersCatalogs (catalogId, manufacturerId, internalLocationId, fulfillerId)
+        VALUES (:catalogId, :manufacturerId, :internalLocationId, :fulfillerId);
+	SET foreign_key_checks=1;
+      ");
+
+      $relational->bindParam(':catalogId', $catalogId);
+      $relational->bindParam(':manufacturerId', $mfgId);
+      $relational->bindParam(':internalLocationId', $intLID);
+      $relational->bindParam(':fulfillerId', $fulfillerId);
+
+      $relational->execute();
+    }
+
+  public function createBin($intLID, $name, $binType, $status) {
+    $stmt = $this->db->prepare("
+      INSERT INTO Bins
+        (internalLocationId, binName, binType, status, fulfillerId)
+      VALUES
+        (:internalLocationId, :binName, :binType, :status, :fulfillerId);
+    ");
+
+    $stmt->bindValue(':internalLocationId', $intLID);
+    $stmt->bindValue(':binName', $name);
+    $stmt->bindValue(':binType', $binType);
+    $stmt->bindValue(':status', $status);
+    $stmt->bindValue(':fulfillerId', $this->getFulfillerIdFromLocationId($intLID));
+
+    $stmt->execute();
+  }
+
+  private function getBin($binName, $fulfillerId, $internalLocationId) {
+    $stmt = $this->db->prepare("
+      SELECT * FROM Bins
+      WHERE binName = :binName
+      AND fulfillerId = :fulfillerId
+      AND internalLocationId = :internalLocationId
+    ");
+
+    $stmt->bindValue(':binName', $binName);
+    $stmt->bindValue(':fulfillerId', $fulfillerId);
+    $stmt->bindValue(':internalLocationId', $internalLocationId);
+
+    $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+
+  private function getFulfillerIdFromLocationId($internalLocationId) {
+    $stmt = $this->db->prepare("SELECT fulfillerId FROM Locations WHERE internalLocationId = :internalLocationId");
+    $stmt->bindParam(':internalLocationId', $internalLocationId);
+    $stmt->execute();
+
+    $fetch = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $fetch['fulfillerId'];
+  }
+
+  public function refreshInventory($items) {
+    // STATEMENTS
+    $stmt1 = $this->db->prepare("
+      INSERT INTO BinContainsProducts
+        (binName, fulfillerId, internalLocationId, productUpc, allocated, onHand)
+      VALUES
+        (:binName, :fulfillerId, :internalLocationId, :productUpc, '0', :onHand)
+    ");
+
+    $stmt2 = $this->db->prepare("INSERT INTO FulfillerCarriesProducts(fulfillerId, productUpc, sku)
+        VALUES(:fulfillerId, :productUpc, :sku)");
+
+    $stmt3 = $this->db->prepare("
+        INSERT INTO LocationSellsProducts (internalLocationId, productUpc, storeSku, safetyStock, ltd)
+        VALUES(:internalLocationId, :productUpc, :storeSku, :safetyStock, :ltd)
+      ");
+
+    // UPDATE INVENTORY FOR EACH ITEM
+    foreach ($items as $item) {
+      $fulfillerId = $this->getFulfillerIdFromLocationId($item['internal_fulfiller_location_id']);
+
+      $stmt1->bindParam(':binName', $item['bin_name']);
+      $stmt1->bindParam(':fulfillerId', $fulfillerId);
+      $stmt1->bindParam(':internalLocationId', $item['internal_fulfiller_location_id']);
+      $stmt1->bindParam(':productUpc', $item['UPC']);
+      $stmt1->bindParam(':onHand', $item['onhand']);
+
+      $stmt2->bindParam(':fulfillerId', $fulfillerId);
+      $stmt2->bindParam(':productUpc', $item['UPC']);
+      $stmt2->bindParam(':sku', $item['SKU']);
+
+      $stmt3->bindParam(':internalLocationId', $item['internal_fulfiller_location_id']);
+      $stmt3->bindParam(':productUpc', $item['UPC']);
+      $stmt3->bindParam(':storeSku', $item['SKU']);
+      $stmt3->bindParam(':safetyStock', $item['safety_stock']);
+      $stmt3->bindParam(':ltd', $item['ltd']);
+
+      // create product if missing
+      if (!$this->getProductFromUpc($item['UPC']))
+        $this->createProduct($item);
+
+      // create bin if missing
+      if (!$this->getBin($item['bin_name'], $fulfillerId, $item['internal_fulfiller_location_id']))
+        $this->createBin($item['internal_fulfiller_location_id'], $item['bin_name'], '', '');
+
+      // execute queries
+      $stmt1->execute();
+      $stmt2->execute();
+      $stmt3->execute();
+    }
+  }
+
+  public function createProduct($product) {
+    if (!$this->getCatalog($product['catalog_id']))
+      $this->createCatalog($product['catalog_id'], $product['mfg_id']);
+
+    $stmt = $this->db->prepare("
+      INSERT INTO Products
+        (upc, catalogId, manufacturerId, name)
+      VALUES
+        (:upc, :catalogId, :manufacturerId, :name);
+    ");
+
+    $stmt->bindParam(':upc', $product['UPC']);
+    $stmt->bindParam(':catalogId', $product['catalog_id']);
+    $stmt->bindParam(':manufacturerId', $product['mfg_id']);
+    $stmt->bindParam(':name', $product['product_name']);
+
+    $stmt->execute();
+  }
+
+  public function createCatalog($catalog_id, $mfg_id) {
+    $stmt = $this->db->prepare("
+      INSERT INTO Catalogs
+        (catalogId, manufacturerId)
+      VALUES
+        (:catalogId, :manufacturerId)
+    ");
+
+    $stmt->bindParam(':catalogId', $catalog_id);
+    $stmt->bindParam(':manufacturerId', $mfg_id);
+
+    $stmt->execute();
+  }
+
+  private function getProductFromUpc($upc) {
+    $stmt = $this->db->prepare("
+      SELECT * FROM Products
+      WHERE upc = :upc
+    ");
+    $stmt->bindParam(':upc', $upc);
+    $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+
+  private function getCatalog($catalog_id) {
+    $stmt = $this->db->prepare("
+      SELECT * FROM Catalogs
+      WHERE catalogId = :catalogId
+    ");
+    $stmt->bindParam(':catalogId', $catalogId);
+    $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+}
+?>
---
+++ project/test-harness.php
@@ -0,0 +1,286 @@
+<?php
+include_once('db.php');
+include_once('team-ross-api.php');
+
+// **********************************************************************
+// Route test harness
+// **********************************************************************
+switch ($argv[1]):
+  case 'seed':
+    db_seed($db);
+    break;
+
+  case 'create':
+    db_create_tables($db);
+    break;
+
+  case 'destroy':
+    db_destroy_tables($db);
+    break;
+
+  case 'empty':
+    db_empty_tables($db);
+    break;
+
+  case 'reset':
+    db_destroy_tables($db);
+    db_create_tables($db);
+    db_seed($db);
+    break;
+
+  default:
+    print "Usage: php test-harness.php create|seed|empty|destroy|reset\n";
+    break;
+
+endswitch;
+
+// **********************************************************************
+// Create Tables
+// **********************************************************************
+function db_create_tables($db) {
+  try {
+    $db->exec("
+      create table Catalogs (
+        catalogId VARCHAR(11) NOT NULL,
+        manufacturerId VARCHAR(11) NOT NULL,
+        constraint catalogs_pk PRIMARY KEY (catalogId, manufacturerId)
+      );
+    ");
+
+    $db->exec("
+      create table Fulfillers (
+        fulfillerId VARCHAR(11) NOT NULL,
+        name VARCHAR(32) NOT NULL,
+        constraint fulfillers_pk PRIMARY KEY (fulfillerId)
+      );
+    ");
+
+    $db->exec("
+      create table Locations (
+        externalLocationId VARCHAR(11) NOT NULL,
+        internalLocationId VARCHAR(11) NOT NULL,
+        fulfillerId VARCHAR(11) NOT NULL,
+        locationType VARCHAR(50),
+        latitude decimal(9,6),
+        longitude decimal(9,6),
+        status VARCHAR(50),
+        safetyStockLimitDefault VARCHAR(10),
+        constraint locations_pk PRIMARY KEY (internalLocationId),
+        constraint locations_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers (fulfillerId)
+      );
+    ");
+
+    $db->exec("
+      create table Products (
+        upc VARCHAR(11) NOT NULL,
+        catalogId VARCHAR(11) NOT NULL,
+        manufacturerId VARCHAR(11) NOT NULL,
+        name VARCHAR(50),
+        constraint products_pk PRIMARY KEY (upc),
+        constraint products_fk FOREIGN KEY (catalogId, manufacturerId) REFERENCES Catalogs (catalogId, manufacturerId)
+      );
+    ");
+
+    $db->exec("
+      create table Bins (
+        internalLocationId VARCHAR(11) NOT NULL,
+        fulfillerId VARCHAR(11),
+        binName VARCHAR(50) NOT NULL,
+        binType VARCHAR(50),
+        status VARCHAR(10),
+        constraint bins_pk PRIMARY KEY (internalLocationId, binName),
+        constraint bins_fk FOREIGN KEY (internalLocationId) REFERENCES Locations (internalLocationId)
+      );
+    ");
+
+    $db->exec("
+      create table LocationOffersCatalogs (
+        catalogId VARCHAR(11) NOT NULL,
+        manufacturerId VARCHAR(11) NOT NULL,
+        internalLocationId VARCHAR(11) NOT NULL,
+        fulfillerId VARCHAR(11),
+        constraint loc_catalog_fk FOREIGN KEY (catalogId, manufacturerId) REFERENCES Catalogs (catalogId, manufacturerId) ON UPDATE CASCADE,
+        constraint loc_location_fk FOREIGN KEY (internalLocationId) REFERENCES Locations (internalLocationId) ON UPDATE CASCADE,
+        constraint loc_pk PRIMARY KEY (catalogId, manufacturerId, internalLocationId)
+      );
+    ");
+
+    $db->exec("
+      create table FulfillerCarriesProducts (
+        fulfillerId VARCHAR(11) NOT NULL,
+        productUpc VARCHAR(11) NOT NULL,
+        sku VARCHAR(11) NOT NULL,
+        constraint fcp_fulfiller_fk FOREIGN KEY (fulfillerId) REFERENCES Fulfillers (fulfillerId),
+        constraint fcp_productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products (upc),
+        constraint fcp_pk PRIMARY KEY (fulfillerId, productUpc)
+      );
+    ");
+
+    $db->exec("
+      create table BinContainsProducts (
+        productUpc VARCHAR(11) NOT NULL,
+        binName VARCHAR(11) NOT NULL,
+        internalLocationId VARCHAR(11) NOT NULL,
+        allocated VARCHAR(6) DEFAULT '0',
+        onHand VARCHAR(6),
+        fulfillerId VARCHAR(11),
+        constraint bcp_productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products (upc),
+        constraint bcp_binname_fk FOREIGN KEY (internalLocationId, binName) REFERENCES Bins (internalLocationId, binName),
+        constraint bcp_location_fk FOREIGN KEY (internalLocationId) REFERENCES Locations (internalLocationId),
+        constraint bcp_pk PRIMARY KEY (productUpc, internalLocationId, binName)
+      );
+    ");
+
+    $db->exec("
+      create table LocationSellsProducts (
+        productUpc VARCHAR(10) NOT NULL,
+        internalLocationId VARCHAR(10) NOT NULL,
+        ltd VARCHAR(10),
+        storeSku VARCHAR(10),
+        safetyStock VARCHAR(10),
+        constraint lsp_productUpc_fk FOREIGN KEY (productUpc) REFERENCES Products (upc),
+        constraint lsp_location_fk FOREIGN KEY (internalLocationId) REFERENCES Locations (internalLocationId),
+        constraint lsp_pk PRIMARY KEY (productUpc, internalLocationId)
+      );
+    ");
+
+    print "Tables created.\n";
+  } catch (PDOException $e) {
+    print "Failed to create tables\n";
+  }
+}
+
+// **********************************************************************
+// Empty Tables
+// **********************************************************************
+function db_empty_tables($db) {
+  try {
+    $db->exec("DELETE FROM LocationOffersCatalogs;");
+    $db->exec("DELETE FROM FulfillerCarriesProducts;");
+    $db->exec("DELETE FROM BinContainsProducts;");
+    $db->exec("DELETE FROM LocationSellsProducts;");
+    $db->exec("DELETE FROM Products;");
+    $db->exec("DELETE FROM Bins;");
+    $db->exec("DELETE FROM Catalogs;");
+    $db->exec("DELETE FROM Locations;");
+    $db->exec("DELETE FROM Fulfillers;");
+
+    print "Tables emptied.\n";
+  }
+  catch (PDOException $e) {
+    print "Failed to empty tables\n";
+  }
+}
+
+// **********************************************************************
+// Destroy Tables
+// **********************************************************************
+function db_destroy_tables($db) {
+  try {
+    $db->exec("DROP TABLE LocationOffersCatalogs;");
+    $db->exec("DROP TABLE FulfillerCarriesProducts;");
+    $db->exec("DROP TABLE BinContainsProducts;");
+    $db->exec("DROP TABLE LocationSellsProducts;");
+    $db->exec("DROP TABLE Products;");
+    $db->exec("DROP TABLE Bins;");
+    $db->exec("DROP TABLE Catalogs;");
+    $db->exec("DROP TABLE Locations;");
+    $db->exec("DROP TABLE Fulfillers;");
+
+    print "Tables destroyed.\n";
+  }
+  catch (PDOException $e) {
+    print "Failed to destroy tables\n";
+  }
+}
+
+// **********************************************************************
+// Seed data from CSV files
+// **********************************************************************
+function db_seed($db) {
+  $api = new TeamRossAPI($db);
+
+  $csv = array();
+  $csv['locations']           = 'data-csv-rfc/fulfiller_locations.csv';
+  $csv['location_bins']       = 'data-csv-rfc/fulfiller_location_bins.csv';
+  $csv['inv_available_bins']  = 'data-csv-rfc/fulfiller_inventory_available_bins.csv';
+  $csv['inv_available']       = 'data-csv-rfc/fulfiller_inventory_available.csv';
+  $csv['inv_not_available']   = 'data-csv-rfc/fulfiller_inventory_not_available.csv';
+
+  // **********************************************************************
+  // Locations
+  // **********************************************************************
+  $data = get_csv_data($csv['locations']);
+  $fulfillerCheckStmt = $db->prepare("SELECT * FROM Fulfillers WHERE fulfillerId = :id");
+
+  foreach($data as &$location) {
+    $fulfillerCheckStmt->bindValue(':id', $location['fulfiller_id']);
+    $fulfillerCheckStmt->execute();
+
+    // Create new fulfiller if we need to
+    if (!$fulfillerCheckStmt->fetch(PDO::FETCH_ASSOC))
+      $api->createFulfiller($location['fulfiller_id'], $location['name']);
+
+    // Create Fulfiller location
+    $api->createFulfillmentLocation(
+      $location['name'],
+      $location['external_fulfiller_location_id'],
+      $location['internal_fulfiller_location_id'],
+      $location['fulfiller_id'],
+      $location['description'],
+      $location['latitude'],
+      $location['longitude'],
+      $location['status'],
+      $location['safety_stock'],
+      $location['mfg_id'],
+      $location['catalog_id']
+    );
+  }
+
+  // **********************************************************************
+  // Bins
+  // **********************************************************************
+  $data = get_csv_data($csv['location_bins']);
+
+  foreach($data as &$bin) {
+    $api->createBin(
+      $bin['internal_fulfiller_location_id'],
+      $bin['bin_name'],
+      $bin['bin_type'],
+      $bin['bin_status']
+    );
+  }
+
+  // **********************************************************************
+  // Inventory
+  // **********************************************************************
+  $data = array();
+  $data[] = get_csv_data($csv['inv_available_bins']);
+  $data[] = get_csv_data($csv['inv_available']);
+  $data[] = get_csv_data($csv['inv_not_available']);
+
+  foreach($data as &$data_chunk)
+    $api->refreshInventory($data_chunk);
+
+  print "Data seeded.\n";
+}
+
+// **********************************************************************
+// Read data from CSV file and return array
+// **********************************************************************
+function get_csv_data($csv_file) {
+  $delimiter = ',';
+  $data = array();
+  $handle = fopen($csv_file, "r");
+
+  if (!$handle) die();
+
+  $header = fgetcsv($handle);
+  while (($line_array = fgetcsv($handle, 4000, $delimiter)) !== false) {
+    $data[] = array_combine($header, $line_array);
+  }
+
+  fclose($handle);
+  return $data;
+}
+?>
