9caceadfa4e666d8f74beef0aeb6000b60490ef7
cricketts <cricketts0@gmail.com>
2013-05-30 14:59:08 -0700

    templates.go now loads all templates

--- src/apersci/output/templates.go
+++ src/apersci/output/templates.go
@@ -5,16 +5,38 @@ import "html/template"
 var templates *template.Template
 
 var templateNames = [...]string{
+	"request-adjustInventory.xml",
+	"request-allocateInventory.xml",
 	"request-createBin.xml",
 	"request-createFulfiller.xml",
 	"request-createFulfillmentLocation.xml",
-	"request-refreshInventory.xml",
+	"request-deallocateInventory.xml",
+	"request-fulfillInventory.xml",
+	"request-getBinStatuses.xml",
+	"request-getBins.xml",
 	"request-getBinTypes.xml",
+	"request-getFulfillerLocations.xml",
+	"request-getFulfillerLocationTypes.xml",
+	"request-getFulfillerStatus.xml",
+	"request-getInventory.xml",
+	"request-getItemLocationsByFulfiller.xml",
+	"request-refreshInventory.xml",
+	"response-adjustInventory.xml",
+	"response-allocateInventory.xml",
 	"response-createBin.xml",
 	"response-createFulfiller.xml",
 	"response-createFulfillmentLocation.xml",
-	"response-refreshInventory.xml",
+	"response-deallocateInventory.xml",
+	"response-fulfillInventory.xml",
+	"response-getBinStatuses.xml",
+	"response-getBins.xml",
 	"response-getBinTypes.xml",
+	"response-getFulfillerLocations.xml",
+	"response-getFulfillerLocationTypes.xml",
+	"response-getFulfillerStatus.xml",
+	"response-getInventory.xml",
+	"response-getItemLocationsByFulfiller.xml",
+	"response-refreshInventory.xml"
 }
 
 func init() {
