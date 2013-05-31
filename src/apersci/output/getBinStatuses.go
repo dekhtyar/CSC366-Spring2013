a2d3d57fc216c5d114f038dc82f5b9d2ef724e47
jcosio <jcosio@calpoly.edu>
2013-05-30 22:49:38 -0700

    updated getBinStatuses now waiting to be tested

--- src/apersci/output/getBinStatuses.go
+++ src/apersci/output/getBinStatuses.go
@@ -2,12 +2,13 @@ package output
 
 import (
 	"io"
+	"apersci/soap"
 )
 
 func GetBinStatusesRequest(w io.Writer, v string) error {
 	return templates.ExecuteTemplate(w, "request-getBinStatuses.xml", v)
 }
 
-func GetBinStatusesResponse(w io.Writer, v string) error {
+func GetBinStatusesResponse(w io.Writer, v soap.GetBinStatusesResponse) error {
 	return templates.ExecuteTemplate(w, "response-getBinStatuses.xml", v)
 }
--- src/apersci/service/getBinStatuses.go
+++ src/apersci/service/getBinStatuses.go
@@ -1,11 +1,48 @@
 package main
 
-import (
-	"fmt"
+import(
+	"apersci/input"
+	"apersci/output"
+	"apersci/soap"
 	"net/http"
 )
 
 func getBinStatuses(w http.ResponseWriter, r *http.Request) (err error) {
-	fmt.Println("TODO")
-	return
+	err = input.GetBinStatusesRequest(r.Body)
+	if err != nil {
+		return
+	}
+
+	conn, err := getDBConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query("SELECT DISTINCT status FROM Bins")
+	if err != nil {
+		return
+	}
+
+    var response soap.GetBinStatusesResponse
+	for rows.Next() {
+		var binStatus string
+		err = rows.Scan(&binStatus)
+		if err != nil {
+			return
+		}
+		response.Return = append(response.Return, binStatus)
+	}
+	err = rows.Err()
+	if err != nil {
+		return
+	}
+
+	err = output.GetBinStatusesResponse(w, response)
+	if err != nil {
+		return
+	}
+
+    return
 }
+
