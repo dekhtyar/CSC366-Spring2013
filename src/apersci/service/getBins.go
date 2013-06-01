6a5d18f4f51746c7324ae82473815c2651229b26
Chase Ricketts <cricketts0@gmail.com>
2013-06-01 15:21:39 -0700

    fixed getBins operation

--- src/apersci/service/getBins.go
+++ src/apersci/service/getBins.go
@@ -4,13 +4,10 @@ import (
 	"apersci/input"
 	"apersci/output"
 	"apersci/soap"
-	"fmt"
 	"net/http"
 )
 
 func getBins(w http.ResponseWriter, r *http.Request) (err error) {
-	fmt.Println("Entering\n")
-
 	req, err := input.GetBinsRequest(r.Body)
 	if err != nil {
 		return
@@ -22,24 +19,20 @@ func getBins(w http.ResponseWriter, r *http.Request) (err error) {
 	}
 	defer conn.Close()
 
-	fmt.Println("Querying (fulfillerID=%d, locationID=%d, Search=%s, numR=%d, start=%d)\n",
-		req.FulfillerID, req.FulfillerLocationID, req.SearchTerm, req.NumResults, req.ResultsStart)
 	rows, err := conn.Query(`
 		SELECT l.FulfillerID, b.ID, l.ID, b.Type, b.Status, b.Name
 		FROM Bins b
 			JOIN Locations l ON (l.Id = b.LocationID)
-		WHERE l.FulfillerID = $2
+		WHERE l.FulfillerID = $1
 			AND l.ID = $2
-			AND b.Name LIKE '%$3%'
-		LIMIT $4 OFFSET $5`,
-		req.FulfillerID, req.FulfillerLocationID, req.SearchTerm, req.NumResults, req.ResultsStart)
-	fmt.Println("here1\n")
+			AND b.Name LIKE '%`+req.SearchTerm+`%'
+		LIMIT $3 OFFSET $4`,
+		req.FulfillerID, req.FulfillerLocationID, req.NumResults, req.ResultsStart)
 	if err != nil {
 		return
 	}
 
-	fmt.Println("here2\n")
-	var response soap.GetBinsResponse
+	var resp soap.GetBinsResponse
 	for rows.Next() {
 		var bin soap.Bin
 		err = rows.Scan(&bin.FulfillerID, &bin.BinID, &bin.FulfillerLocationID,
@@ -48,21 +41,19 @@ func getBins(w http.ResponseWriter, r *http.Request) (err error) {
 			return
 		}
 
-		response.Return.Bins = append(response.Return.Bins, bin)
+		resp.Return.Bins = append(resp.Return.Bins, bin)
 	}
 	err = rows.Err()
-	fmt.Println("here3\n")
 	if err != nil {
 		return
 	}
 
-	fmt.Println("converting\n")
-	err = output.GetBinResponse(w, response)
+	resp.Return.ResultCount = len(resp.Return.Bins)
+
+	err = output.GetBinResponse(w, resp)
 	if err != nil {
 		return
 	}
-
-	fmt.Println("Returning\n")
 
 	return
 }
