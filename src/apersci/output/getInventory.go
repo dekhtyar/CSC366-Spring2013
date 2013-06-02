5fd6f7d74480212ce30bfd1cbfcc25e876ec4dc5
Chase Ricketts <cricketts0@gmail.com>
2013-06-02 12:43:59 -0700

    added initial portion for getInventory operation

--- src/apersci/output/getInventory.go
+++ src/apersci/output/getInventory.go
@@ -9,6 +9,6 @@ func GetInventoryRequest(w io.Writer, v soap.InventoryRequest) error {
 	return templates.ExecuteTemplate(w, "request-getInventory.xml", v)
 }
 
-func GetInventoryResponse(w io.Writer, v string) error {
+func GetInventoryResponse(w io.Writer, v soap.GetInventoryResponse) error {
 	return templates.ExecuteTemplate(w, "response-getInventory.xml", v)
 }
--- src/apersci/service/getInventory.go
+++ src/apersci/service/getInventory.go
@@ -1,11 +1,102 @@
 package main
 
 import (
+	"apersci/input"
+	"apersci/output"
+	"apersci/soap"
 	"fmt"
 	"net/http"
 )
 
+/*
+From the Inventory-Spec-v2.pdf:
+
+There are a few use modes for the Get Inventory request. However, there are some common elements
+to all requests. All get inventory requests involve:
+	• A manufacturer id and catalog id
+	• One or more SKU/UPC combinations, with a minimum on-hand stock threshold.
+	• Limited to one type of store location.
+	• An upper limit on how many locations should be returned.
+	• A flag on if safety stock should be enforced.
+	• A flag on if negative available inventory should be included.
+	• A flag to order by LTD or by quantity available.
+
+The variations of the Get Inventory request are:
+	1. Limit the results to one or more locations, identified by name.
+	2. Limit the results to locations within a specific distance radius of a given Latitude and Longitude.
+	3. Limit the results to locations within a specific distance radius of a given zip code.
+*/
+
 func getInventory(w http.ResponseWriter, r *http.Request) (err error) {
-	fmt.Println("TODO")
+	fmt.Println("In getInventory\n")
+
+	req, err := input.GetInventoryRequest(r.Body)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("get connection\n")
+	conn, err := getDBConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	/*
+				FulfillerID              uint
+		153     ManufacturerID           uint            `xml:"Catalog>ManufacturerID"`
+		154     CatalogID                uint            `xml:"Catalog>CatalogID"`
+		155     Quantities               []InventoryItem `xml:">items"`
+		156     LocationNames            []string        `xml:">LocationNames"`
+		157     Location                 Location
+		158     Type                     string
+		159     Limit                    int
+		160     IgnoreSafetyStock        bool
+		161     IncludeNegativeInventory bool
+		162     OrderByLTD               bool
+	*/
+	req = req
+	var resp soap.GetInventoryResponse
+	/* TODO
+	rows, err := conn.Query(`
+		SELECT l.FulfillerID, b.ID, l.ID, b.Type, b.Status, b.Name
+		FROM Bins b
+			JOIN Locations l ON (l.Id = b.LocationID)
+		WHERE l.FulfillerID = $1
+			AND l.ID = $2
+			AND b.Name LIKE '%`+req.SearchTerm+`%'
+		LIMIT $3 OFFSET $4`,
+		req.FulfillerID, req.FulfillerLocationID, req.NumResults, req.ResultsStart)
+	if err != nil {
+		return
+	}
+
+	var resp soap.GetBinsResponse
+	for rows.Next() {
+		var bin soap.Bin
+		err = rows.Scan(&bin.FulfillerID, &bin.BinID, &bin.FulfillerLocationID,
+			&bin.BinType, &bin.BinStatus, &bin.Name)
+		if err != nil {
+			return
+		}
+
+		resp.Return.Bins = append(resp.Return.Bins, bin)
+	}
+	err = rows.Err()
+	if err != nil {
+		return
+	}
+
+	resp.Return.ResultCount = len(resp.Return.Bins)
+
+	*/
+
+	fmt.Println("converting\n")
+	err = output.GetInventoryResponse(w, resp)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Returning\n")
 	return
 }
