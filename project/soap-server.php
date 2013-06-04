e3723b851aece004bd7a7e5c3e979472a8a6f34b
rossmckelvie <r@rossmckelvie.com>
2013-06-03 22:59:29 -0700

    handle data from php stream

--- project/soap-server.php
+++ project/soap-server.php
@@ -1,6 +1,8 @@
 <?php
 include_once("team-ross-soap.php");
 
+$data = file_get_contents('php://input');
+
 $server = new SoapServer("core.wsdl");
 $server->setClass('TeamRossSOAP');
-$server->handle();
+$server->handle($data);
