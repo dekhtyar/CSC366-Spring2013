10e77831b5516450ffe446d8ed698ee0c3ef6dd4
Jess <jcosio@calpoly.edu>
2013-06-06 09:39:44 -0700

    fixed parameters for response

--- src/apersci/output/getFulfillmentLocationTypes.go
+++ src/apersci/output/getFulfillmentLocationTypes.go
@@ -5,10 +5,10 @@ import (
 	"io"
 )
 
-func GetFulfillmentLocationTypesRequest(w io.Writer, v soap.FulfillmentLocation) error {
+func GetFulfillmentLocationTypesRequest(w io.Writer, v string) error {
 	return templates.ExecuteTemplate(w, "request-createFulfillmentLocation.xml", v)
 }
 
-func GetFulfillmentLocationTypesResponse(w io.Writer, v string) error {
+func GetFulfillmentLocationTypesResponse(w io.Writer, v soap.GetFulfillerLocationTypesResponse) error {
 	return templates.ExecuteTemplate(w, "response-getFulfillmentLocation.xml", v)
 }
