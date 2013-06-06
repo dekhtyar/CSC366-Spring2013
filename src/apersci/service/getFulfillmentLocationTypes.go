4534fa914023b2ddb7183c722ccc6974ae42c708
Jess <jcosio@calpoly.edu>
2013-06-06 09:18:27 -0700

    added getFulfillmentLocationTypes

--- src/apersci/service/getFulfillmentLocationTypes.go
+++ src/apersci/service/getFulfillmentLocationTypes.go
@@ -1,11 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"apersci/input"
+	"apersci/output"
+	"apersci/soap"
 	"net/http"
 )
 
 func getFulfillmentLocationTypes(w http.ResponseWriter, r *http.Request) (err error) {
-	fmt.Println("TODO")
+	err = input.GetFulfillmentLocationTypesRequest(r.Body)
+	if err != nil {
+		return
+	}
+
+	conn, err := getDBConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query("SELECT DISTINCT type FROM locations")
+	if err != nil {
+		return
+	}
+
+	var response soap.GetFulfillerLocationTypesResponse
+	for rows.Next() {
+		var fulfillmentLocationType string
+		err = rows.Scan(&fulfillmentLocationType)
+		if err != nil {
+			return
+		}
+		response.Return = append(response.Return, fulfillmentLocationType)
+	}
+	err = rows.Err()
+	if err != nil {
+		Return
+	}
+
+	err = output.GetFulfillmentLocationTypesResponse(w, response)
+	if err != nil {
+		return
+	}
+
 	return
 }
