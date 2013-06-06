afdac14041059d7a84438b3f819133d00ced2ca8
Cait Rahm <crahm@calpoly.edu>
2013-06-06 10:54:25 -0700

    added service

--- src/apersci/service/getFulfillerStatus.go
+++ src/apersci/service/getFulfillerStatus.go
@@ -1,11 +1,58 @@
 package main
 
 import (
+	"apersci/input"
+	"apersci/output"
 	"fmt"
 	"net/http"
 )
 
 func getFulfillerStatus(w http.ResponseWriter, r *http.Request) (err error) {
-	fmt.Println("TODO")
+	l, err := input.GetFulfillerStatusRequest(r.Body)
+	if err != nil {
+		return
+	}
+
+	conn, err := getDBConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	tx, err := getDBTransaction()
+	if err != nil {
+		return
+	}
+	defer tx.Rollback()
+
+	rows, err := conn.Query("SELECT COUNT(*) FROM Locations WHERE fulfillerId = $1 AND status = 'active'",
+		l)
+	if err != nil {
+		return
+	}
+
+	count, err := readIntAndCloseRows(rows)
+	if err != nil {
+		return
+	}
+
+	var str string
+
+	if count > 0 {
+		str = "active"
+	} else {
+		str = "inactive"
+	}
+
+	err = output.GetFulfillerStatusResponse(w, fmt.Sprint(str))
+	if err != nil {
+		return
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return
+	}
+
 	return
 }
