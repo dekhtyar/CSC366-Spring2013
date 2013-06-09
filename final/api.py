5ac6cdec2e63ec5f2fb5a51036b0c5a3e2b89441
Alex Boltunov <aboltunov@gmail.com>
2013-06-08 17:27:50 -0700

    Fixed allocateInventory issues

--- final/api.py
+++ final/api.py
@@ -291,7 +291,21 @@ def getFulfillmentLocationTypes(fID, fLID, db):
    except Exception, e:
      print e
 
-def itemAvailable(fulfuller_id, location_id, item, db):
+def selectItemBins(fulfiller_id, location_id, sku, db):
+    cur = db.cur()
+
+    try:
+        cur.execute('SELECT OnHand, Allocated, Name ' +
+                    'FROM StoredIn WHERE SKU = %s AND FulfillerId = %s AND FulfillerLocationId = %s',
+                    (sku, fulfiller_id, location_id))
+        return cur.fetchall()
+    except Exception, e:
+        print e
+        return () 
+    finally:
+        cur.close()
+
+def itemCanBeAllocated(fulfuller_id, location_id, item, db):
     cur = db.cur()
     onhand = 0
 
@@ -300,35 +314,52 @@ def itemAvailable(fulfuller_id, location_id, item, db):
                     (item['SKU'], fulfiller_id, location_id))
         safety = cur.fetchone()
 
-        cur.execute('SELECT OnHand FROM StoredIn WHERE SKU = %s AND FulfillerId = %s AND FulfillerLocationId = %s',
-                    (item['SKU'], fulfiller_id, location_id))
-        for i in range(cur.rowcount):
-            onhand = onhand + cur.fetchone()[0]
+        for bin in selectItemBins(fulfiller_id, location_id, item['SKU'], db):
+            onhand = onhand + bin[0] 
 
-        if item['quantity'] >= (onhand - safety):
+        if item['quantity'] <= (onhand - safety):
             return True
     except Exception, e:
         print e
+    finally:
+        cur.close()
 
     return False
 
-def allocateItem(fulfiller_id, location_id, item, db):
-    cur = db.cur()
-    quantity = item['quantity']
+def allocateFromBin(fulfiller_id, location_id, sku, quantity, name, db):
+    cur = db.cursor()
 
     try:
         cur.execute('UPDATE StoredIn' +
                     'SET OnHand = OnHand - %s, Allocated = Allocated + %s' +
-                    'WHERE SKU = %s AND FulfillerId = %s AND FulfillerLocationId = %s',
-                    (quantity, quantity, item['SKU'], fulfiller_id, location_id))
+                    'WHERE SKU = %s AND FulfillerId = %s AND FulfillerLocationId = %s AND Name = %s',
+                    (quantity, quantity, item['SKU'], fulfiller_id, location_id, name))
     except Exception, e:
         print e
+    finally:
+        cur.close()
+
+def allocateItem(fulfiller_id, location_id, item, db):
+    cur = db.cur()
+    quantity = item['quantity']
+    bins = selectItemBins(fulfiller_id, location_id, sku, db)
+
+    for bin in bins:
+        onhand = bin[0]
+        name = bin[2]
+        allocation = quantity if onhand >= quantity else onhand
+
+        allocateFromBin(fulfiller_id, location_id, sku, allocation, name, db)
+        quantity = quantity - allocation
+        
+        if quantity == 0:
+            return
 
 def allocateInventory(fulfiller_id, catalog_id, location_id, items, db):
     cur = db.cur()
 
     for item in items:
-        if not itemAvailable(fulfiller_id, location_id, item, db):
+        if not itemCanBeAllocated(fulfiller_id, location_id, item, db):
             print 'Insufficient available inventory. Aborting...'
             return
 
@@ -336,7 +367,7 @@ def allocateInventory(fulfiller_id, catalog_id, location_id, items, db):
         allocateItem(fulfiller_id, location_id, item, db)
 
 def deallocateInventory(fulfiller_id, catalog_id, location_id, items, db):
-    cursor = db.cursor()
+    cur = db.cursor()
 
 def fulfillInventory(fulfiller_id, catalog_id, location_id, items, db):
-    cursor = db.cursor()
+    cur = db.cursor()
