24e49ee260de312a8f6e67dbc6b94de0af0f7e0e
Jess <jcosio@calpoly.edu>
2013-06-10 17:32:48 -0700

    changed getfulfillmentlocationtypes to getfulfillerlocationtypes

--- src/apersci/output/getFulfillmentLocationTypes.go
+++ src/apersci/output/getFulfillmentLocationTypes.go
@@ -6,9 +6,9 @@ import (
 )
 
 func GetFulfillmentLocationTypesRequest(w io.Writer, v string) error {
-	return templates.ExecuteTemplate(w, "request-createFulfillmentLocation.xml", v)
+	return templates.ExecuteTemplate(w, "request-createFulfillerLocationTypes.xml", v)
 }
 
 func GetFulfillmentLocationTypesResponse(w io.Writer, v soap.GetFulfillerLocationTypesResponse) error {
-	return templates.ExecuteTemplate(w, "response-getFulfillmentLocation.xml", v)
+	return templates.ExecuteTemplate(w, "response-getFulfillerLocationTypes.xml", v)
 }
