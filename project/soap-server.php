724cb1a053ce73a7a8db39b4efb7f40274ce4c54
rossmckelvie <r@rossmckelvie.com>
2013-06-12 20:10:32 -0700

    allow cross origin requests

--- project/soap-server.php
+++ project/soap-server.php
@@ -3,6 +3,8 @@ include_once("team-ross-soap.php");
 
 $data = file_get_contents('php://input');
 
+header('Access-Control-Allow-Origin: *');
+
 $server = new SoapServer("core.wsdl");
 $server->setClass('TeamRossSOAP');
 $server->handle($data);
